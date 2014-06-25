02e6a70aa3183ba012022146dac793b62415d328
Ian Bell <ian.h.bell@gmail.com>
2014-06-26 01:50:20 +0200

    Updated FORTRAN 77 wrapper - works at a rudimentary level, just need to re-enable other functions
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

--- wrappers/Fortran/build.bat
+++ wrappers/Fortran/build.bat
@@ -1,13 +1,5 @@
 @echo off
 REM Some Windows commands to compile the Fortran interface for F77
-setlocal 
-set comPath=C:\MinGW32-xy\bin
-set FC=%comPath%\gfortran
-set CC=%comPath%\gcc
-REM set comPath=C:\MinGW32\bin
-REM set FC=%comPath%\g77
-REM set CC=%comPath%\gcc
-%FC% -g -c examplef.for
-%CC% -g -c coolpropIF.c 
-%FC% -g -o example examplef.o coolpropIF.o -L../SharedLibrary -lCoolProp
-endlocal 
+copy C:\Users\Belli\Documents\Code\coolprop-v5-new\build\32bitcdecl\Debug\Coolprop.dll .
+C:\MinGW\bin\gfortran -g -o example examplef.for -L. -lCoolProp
+example
--- wrappers/Fortran/examplef.for
+++ wrappers/Fortran/examplef.for
@@ -1,8 +1,7 @@
 ! Example fortran file 
 ! 
       double precision T,Q,D,h,s
-      character(LEN=32) Ref
-      character(LEN=2) Output, Name1, Name2
+      character(LEN=32) Ref,Output, Name1, Name2
       double precision outVal,Prop1,Prop2
 
       integer inte
@@ -17,6 +16,7 @@
       Name2  = "Q"//CHAR(0)
       Prop2  = Q
       Ref    = "R134a"//CHAR(0)
+      outval = 9999999
 
 !       Output(LEN(Output):LEN(Output)) = CHAR(0)
 !       Name1(LEN(Name1):LEN(Name1)) = CHAR(0)
@@ -24,44 +24,44 @@
 !       Ref(LEN(Ref):LEN(Ref)) = CHAR(0)
 
       write(*,*) "Saturation pressure for R134a: "
-      outVal = props(Output, Name1, Prop1, Name2, Prop2, Ref)
+      call propssi(Output, Name1, Prop1, Name2, Prop2, Ref, outVal)
       write(*,*) "Result was: ", outVal/1e2, " bar"
       write(*,*) "-----------------------------------------------"
-
-      Output = "S"//CHAR(0)
-
-
-
-      outVal = props1(Ref , "Tcrit"//CHAR(0))
-      write(*,*) "Tcrit from props1     : ", outVal 
-      outVal = props(Output, Name1, Prop1, Name2, Prop2, Ref)
-      write(*,*) "S from standard props : ", outVal
-      outVal = 0.0
-      CALL propsmod(Output, Name1, Prop1, Name2, Prop2, Ref, outVal)
-      write(*,*) "S from modified props : ", outVal
-      outVal = derivterms("dpdrho"//CHAR(0), Prop1, D, Ref)
-      write(*,*) "dpdrho from derivterms: ", outVal
-      inte = setreferencestates(Ref, "IIR"//CHAR(0))
-      write(*,*) "reference to IIR      : ", inte
-      outVal = props(Output, Name1, Prop1, Name2, Prop2, Ref)
-      write(*,*) "S from standard props : ", outVal
-      inte = setreferencestates(Ref, "ASHRAE"//CHAR(0))
-      write(*,*) "reference to ASHRAE   : ", inte
-      outVal = props(Output, Name1, Prop1, Name2,Prop2, Ref)
-      write(*,*) "S from standard props : ", outVal
-      inte = enablettselut(Ref)
-      write(*,*) "enabling TTSE         : ", inte
-      inte = isenabledttselut(Ref)
-      write(*,*) "state of TTSE         : ", inte
-      outVal = props(Output, Name1, Prop1, Name2, Prop2, Ref)
-      write(*,*) "S from TTSE props     : ", outVal
-      inte = setttsemode(Ref , "bicubic"//CHAR(0))
-      write(*,*) "TTSE mode to bicubic  : ", inte
-      outVal = props(Output, Name1, Prop1, Name2, Prop2, Ref)
-      write(*,*) "S from bicubic props  : ", outVal
-      inte = disablettselut(Ref)
-      write(*,*) "Disabling TTSE        : ", inte
-      outVal = props(Output, Name1, Prop1, Name2, Prop2, Ref)
-      write(*,*) "S from standard props : ", outVal
+c
+c      Output = "S"//CHAR(0)
+c
+c
+c
+c      outVal = props1(Ref , "Tcrit"//CHAR(0))
+c      write(*,*) "Tcrit from props1     : ", outVal 
+c      outVal = props(Output, Name1, Prop1, Name2, Prop2, Ref)
+c      write(*,*) "S from standard props : ", outVal
+c      outVal = 0.0
+c      CALL propsmod(Output, Name1, Prop1, Name2, Prop2, Ref, outVal)
+c      write(*,*) "S from modified props : ", outVal
+c      outVal = derivterms("dpdrho"//CHAR(0), Prop1, D, Ref)
+c      write(*,*) "dpdrho from derivterms: ", outVal
+c      inte = setreferencestates(Ref, "IIR"//CHAR(0))
+c      write(*,*) "reference to IIR      : ", inte
+c      outVal = props(Output, Name1, Prop1, Name2, Prop2, Ref)
+c      write(*,*) "S from standard props : ", outVal
+c      inte = setreferencestates(Ref, "ASHRAE"//CHAR(0))
+c      write(*,*) "reference to ASHRAE   : ", inte
+c      outVal = props(Output, Name1, Prop1, Name2,Prop2, Ref)
+c      write(*,*) "S from standard props : ", outVal
+c      inte = enablettselut(Ref)
+c      write(*,*) "enabling TTSE         : ", inte
+c      inte = isenabledttselut(Ref)
+c      write(*,*) "state of TTSE         : ", inte
+c      outVal = props(Output, Name1, Prop1, Name2, Prop2, Ref)
+c      write(*,*) "S from TTSE props     : ", outVal
+c      inte = setttsemode(Ref , "bicubic"//CHAR(0))
+c      write(*,*) "TTSE mode to bicubic  : ", inte
+c      outVal = props(Output, Name1, Prop1, Name2, Prop2, Ref)
+c      write(*,*) "S from bicubic props  : ", outVal
+c      inte = disablettselut(Ref)
+c      write(*,*) "Disabling TTSE        : ", inte
+c      outVal = props(Output, Name1, Prop1, Name2, Prop2, Ref)
+c      write(*,*) "S from standard props : ", outVal
 
       end program 
