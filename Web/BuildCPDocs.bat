76e49a3219f23226ea74d4ddb64977798c51ee78
Ian Bell <ian.h.bell@gmail.com>
2014-07-07 20:25:49 +0200

    sphinx-apidoc does not make top-level modules.rst file anymore
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

--- Web/BuildCPDocs.bat
+++ Web/BuildCPDocs.bat
@@ -9,5 +9,5 @@ REM ~ pdflatex CoolPropdoc.tex
 REM ~ copy /Y CoolPropdoc.pdf ..\..\_static\
 REM ~ cd ..\..
 
-rem sphinx-apidoc -f -o apidoc ../CoolProp
+rem sphinx-apidoc -T -f -o apidoc ../CoolProp
 mingw32-make html
