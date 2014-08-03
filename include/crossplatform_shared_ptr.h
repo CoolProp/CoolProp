18899a5cc3ab0857e379f4ec4168bc2403f0ecf2
Ian Bell <ian.h.bell@gmail.com>
2014-08-03 14:29:07 +0200

    Fixed files to compile for python on OSX

--- include/crossplatform_shared_ptr.h
+++ include/crossplatform_shared_ptr.h
@@ -6,22 +6,25 @@
 // Based on the platform and compiler, include the necessary header to give access to std::tr1::shared_ptr directly as shared_ptr
 
 #if defined(__ISLINUX__) && (defined(__llvm__) || defined(__clang__))
-#include <memory>
-using std::shared_ptr;
+    #include <memory>
+    using std::shared_ptr;
 #elif defined(__ISLINUX__)
-#include <tr1/memory>
-using namespace std::tr1;
+    #include <tr1/memory>
+    using namespace std::tr1;
+#elif defined(__GNUC__)
+    #include <tr1/memory>
+    using namespace std::tr1;
 #elif defined(__ISAPPLE__)
-#include <memory>
-using std::shared_ptr;
+    #include <memory>
+    using std::shared_ptr;
 #elif defined(__ISWINDOWS__) && defined(__MINGW32__)
-#include <tr1/memory>
-using namespace std::tr1;
+    #include <tr1/memory>
+    using namespace std::tr1;
 #elif defined(__ISWINDOWS__) && !defined(__MINGW32__)
-#include <memory>
-using namespace std::tr1;
+    #include <memory>
+    using namespace std::tr1;
 #else
-#pragma error
+    #pragma error
 #endif
 
 
