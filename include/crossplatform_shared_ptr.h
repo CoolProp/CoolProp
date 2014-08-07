a3a122c6ebdd5f852d0389507abcac10b3e17ab3
Ian Bell <ian.h.bell@gmail.com>
2014-08-07 02:50:13 +0200

    Finally comprehensively fixed the shared_ptr header - more cleaning can be done I guess, but most cases are covered here I think.

--- include/crossplatform_shared_ptr.h
+++ include/crossplatform_shared_ptr.h
@@ -5,16 +5,24 @@
 
 // Based on the platform and compiler, include the necessary header to give access to std::tr1::shared_ptr directly as shared_ptr
 
-#if defined(__ISLINUX__) && (defined(__llvm__) || defined(__clang__))
+#if defined(__ISLINUX__) && (defined(__llvm__) || defined(__clang__)) // CLANG
     #include <memory>
     using std::shared_ptr;
-#elif defined(__ISLINUX__)
+#elif defined(__ISLINUX__) // GCC
     #include <tr1/memory>
     using namespace std::tr1;
-#elif defined(__ISAPPLE__) && (defined(__llvm__) || defined(__clang__))
-    // See also http://stackoverflow.com/questions/13445742/apple-and-shared-ptr
-    #include <memory>
-    using std::shared_ptr;
+#elif defined(__ISAPPLE__) && (defined(__llvm__) || defined(__clang__)) // CLANG
+    // See docs for clang: http://clang.llvm.org/docs/LanguageExtensions.html#include-file-checking-macros
+    #if __has_include(<tr1/memory>)
+        // CLANG and -stdlib=libstdc++
+        // See also http://stackoverflow.com/questions/13445742/apple-and-shared-ptr
+        #include <tr1/memory>
+        using namespace std::tr1;
+    #else
+        // CLANG and -stdlib=libc++
+        #include <memory>
+        using std::shared_ptr;
+    #endif
 #elif defined(__GNUC__)
     #include <tr1/memory>
     using namespace std::tr1;
