81c6abca5730f99c1978842568617f9f3877ef48
Ian Bell <ian.h.bell@gmail.com>
2014-09-14 23:44:12 +0200

    Added build script for swig+scilab

--- dev/scripts/build_swig_scilab.py
+++ dev/scripts/build_swig_scilab.py
@@ -9,6 +9,6 @@ if not glob.glob('pcre-*.tar.gz'):
 	wget.download('ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.33.tar.gz')
 subprocess.check_call('Tools/pcre-build.sh', shell = True, stdout = sys.stdout, stderr = sys.stderr)
 subprocess.check_call('./autogen.sh', shell = True, stdout = sys.stdout, stderr = sys.stderr)
-subprocess.check_call('./configure --disable-ccache --with-scilab-inc=${SCILAB_HOME}/include --with-scilab=${SCILAB_HOME}/bin/scilab-cli --prefix=${PWD}/../swig-scilab-bin', shell = True, stdout = sys.stdout, stderr = sys.stderr)
+subprocess.check_call('./configure --disable-ccache --with-scilab-inc=${SCILAB_HOME}/include --with-scilab=${SCILAB_HOME}/bin/scilab-cli --prefix=${PWD}/swig-scilab-bin', shell = True, stdout = sys.stdout, stderr = sys.stderr)
 subprocess.check_call('make', shell = True, stdout = sys.stdout, stderr = sys.stderr)
 subprocess.check_call('make install', shell = True, stdout = sys.stdout, stderr = sys.stderr)
