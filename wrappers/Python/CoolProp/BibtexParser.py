bead14f8e24913a4f60700b6e02f3c7ff28f2a32
Jorrit Wronski <jowr@mek.dtu.dk>
2014-09-16 18:43:39 +0200

    The Bibtexer is back

--- wrappers/Python/CoolProp/BibtexParser.py
+++ wrappers/Python/CoolProp/BibtexParser.py
@@ -2,10 +2,14 @@
 # -*- coding: utf8 -*-
 
 from __future__ import division, absolute_import, print_function
+from __future__ import generators
 from pybtex.database.input import bibtex
 from pybtex.plugin import find_plugin
 import io
-
+import latexcodec.codec
+import copy
+import string
+from pybtex.database import FieldDict
 
 
 class BibTeXerClass(object):
@@ -14,8 +18,10 @@ class BibTeXerClass(object):
     to print a very basic bibliography.
     """
 
-    def __init__(self, fName = '../../../CoolPropBibTeXLibrary.bib'):
+    def __init__(self, fName = u'../../../CoolPropBibTeXLibrary.bib'):
         self.DEBUG = False
+
+        latexcodec.codec.register()
 
         self.sanitizeDict = {}
         self.sanitizeDict['year']        = "no year"
@@ -28,7 +34,6 @@ class BibTeXerClass(object):
         self.sanitizeDict['note']        = "no note"
         self.sanitizeDict['publisher']   = "no publisher"
         self.sanitizeDict['institution'] = "no institution"
-
         self.loadLibrary(fName)
 
 
@@ -51,164 +56,201 @@ class BibTeXerClass(object):
 
 
 
-    def getEntry(self, key):
+    ########################################
+    # Find entry in library and make unicode
+    ########################################
+    def getUnicodeEntry(self, key):
         if self.library is None:
             raise ValueError("Library has not been loaded.")
-        if key in self.library.entries.keys():
-            return self.library.entries[key]
+        entry = copy.deepcopy(self.library.entries[key])
+
+        for key, value in entry.fields.iteritems():
+            entry.fields[key] = self.fromLatex(value)
+
+        for key in entry.persons.keys():
+            for i in range(len(entry.persons[key])):
+                entry.persons[key][i]._first = self.fromLatex(entry.persons[key][i].first())
+                entry.persons[key][i]._middle = self.fromLatex(entry.persons[key][i].middle())
+                entry.persons[key][i]._prelast = self.fromLatex(entry.persons[key][i].prelast())
+                entry.persons[key][i]._last = self.fromLatex(entry.persons[key][i].last())
+                entry.persons[key][i]._lineage = self.fromLatex(entry.persons[key][i].lineage())
+
+        return entry
+
+
+    ########################################
+    # Basic text processing functions
+    ########################################
+
+    def stripCurls(self, text):
+        table = { ord(u'{'): None, ord(u'}'): None }
+        stripped = text.translate(table)
+        if self.DEBUG: print(u"From {0} to {1}".format(text, stripped))
+        return stripped
+
+
+    def fromLatex(self, latexStr):
+        """Converter for Latex strings
+
+        A function that always returns unicode. It also tries to recurse into
+        lists and dicts, but be careful this is not tested.
+        """
+        if isinstance(latexStr, str): # ordinary string
+            latexStr = unicode(latexStr)
+        elif isinstance(latexStr, unicode): # unicode string
+            pass
         else:
-            return "Key {0} not in library.".format(key)
+            try:
+                for k in latexStr.keys():
+                    latexStr[k] = self.fromLatex(latexStr[k])
+            except AttributeError:
+                pass
+            # Check for list
+            if type(latexStr)==type([]):
+                for i in range(len(latexStr)):
+                    latexStr[i] = self.fromLatex(latexStr[i])
+            # return the plain object
+            return latexStr
+
+        if self.DEBUG: print(u"From {0} ".format(latexStr),end=u"")
+        decoded = latexStr.decode('latex')
+        if self.DEBUG: print(u"to {0} ".format(decoded))
+        return decoded
+
 
     #######################################
-    # Some of Ian's old functions
+    # Custom output routines
     #######################################
-    def accent_substitutions(self, name):
-        mapping = [('{\\~n}','\xf1'), # �
-                   ('{\\`e}','\xe8'), # �
-                   ("{\\'e}",'\xe9'), # �
-                   ("{\\'a}",'\xe1'), # �
-                   ("{\\`a}",'\xe0'), # �
-                   ("{\\'i}",'\xed'), # �
-                   ("{\\'i}",'\xed'), # �
-                   ('{\\\"o}','\xf6'), # �
-                   ('{\\\"u}','\xfc'), # �
-                   ('{\\v s}','\x161'), # �
-                   ]
-        for old, new in mapping:
-            name = name.replace(old, new)
-        return name
 
-    def count_substr(self, s, ss):
-        c = 0
-        for e in s:
-            if e == ss:
-                c += 1
-        return c
+    def prepareSingleEntry(self, key):
 
-    def DE(self, s):
-        try:
-            return s.decode('ascii').encode('utf-8')
-        except UnicodeEncodeError:
-            print('Decoding error for',s)
+        # TODO: What is this for?
+        if key.startswith('__'):
+            return None,None
+
+        entry = self.getUnicodeEntry(key)
+        authors = u'; '.join( unicode(author) for author in entry.persons['author'] )
+        authors = self.stripCurls(authors)
+
+        # Strip off the opening and closing brackets
+        for key in entry.fields:
+            if entry.fields[key].startswith('{') and entry.fields[key].endswith('}'):
+                entry.fields[key] = entry.fields[key][1:len(entry.fields[key])-1]
+
+        # Use the basic dictionary and update it with existing values
+        #entry.fields = FieldDict(self.sanitizeDict).update(entry.fields)
+        for key in self.sanitizeDict:
+            if not key in entry.fields:
+                entry.fields[key] = self.sanitizeDict[key]
+
+        return authors,entry
+
+
+    def entry2txt(self, key):
+
+        authors,entry = self.prepareSingleEntry(key)
+        if authors is None:
+            return None
+        else:
+            year        = entry.fields['year']
+            title       = entry.fields['title']
+            journal     = entry.fields['journal']
+            volume      = entry.fields['volume']
+            pages       = entry.fields['pages']
+            booktitle   = entry.fields['booktitle']
+            school      = entry.fields['school']
+            note        = entry.fields['note']
+            publisher   = entry.fields['publisher']
+            institution = entry.fields['institution']
+
+            if entry.type == 'article':
+                return authors + ', ' + year + ', ' + title + ', ' + journal + ', ' + volume + ':' + pages
+            elif entry.type == 'conference':
+                return authors + ', ' + year + ', ' + title + ', ' + booktitle
+            elif entry.type == 'mastersthesis' or entry.type == 'phdthesis':
+                return authors + ', ' + year + ', ' + title + ', ' + school
+            elif entry.type == 'unpublished':
+                return authors + ', ' + year + ', ' + title + ', note: ' + note
+            elif entry.type == 'book':
+                return authors + ', ' + year + ', ' + title + ', ' + publisher
+            elif entry.type == 'techreport':
+                return authors + ', ' + year + ', ' + title + ', ' + institution
+            else:
+                print(u"There was an error processing: {0}".format(entry))
+                return None
 
 
     def entry2rst(self, key):
 
-        if key.startswith('__'):
-            return ''
-
-        entry = self.getEntry(key)
-
-        if entry is None:
-            return ''
-
-        try:
-            authors = '; '.join([self.accent_substitutions(unicode(author).decode('ascii').encode('utf-8')) for author in entry.persons['author']])
-        except UnicodeEncodeError:
-            print('Decoding error for',[author for author in entry.persons['author']])
-
-        if authors.find('{') > -1 or authors.find('}') > -1:
-            print(authors)
-            raise ValueError("authors [{authors:s}] may not have '{{' or '}}' character".format(authors = authors))
-
-        fields = entry.fields
-
-        # Strip off the opening and closing brackets
-        fields['title'] = fields['title'].strip()
-        if fields['title'].startswith('{') and fields['title'].endswith('}'):
-            fields['title'] = fields['title'][1:len(entry.fields['title'])-1]
-
-        f = fields
-        for key in f:
-            f[key] = self.DE(f[key])
-        authors = str(authors)
-
-        if entry.type == 'article':
-            if 'journal' not in fields: fields['journal'] = ''
-            if 'volume' not in fields: fields['volume'] = ''
-            if 'pages' not in fields: fields['pages'] = ''
-
-            return authors + ', ' + f['year'] + ', ' + f['title'] + ', *' + f['journal'] + '*, ' + f['volume'] + ':' + f['pages']
-
-        elif entry.type == 'conference':
-            if 'journal' not in f: f['journal'] = ''
-            return authors + ', ' + f['year'] + ', ' + f['title'] + ', *' + f['booktitle'] + '*'
-
-        elif entry.type == 'mastersthesis':
-            return authors + ', ' + f['year'] + ', ' + f['title'] + ', *' + f['school'] + '*'
-
-        elif entry.type == 'unpublished':
-            return authors + ', ' + f['year'] + ', ' + f['title'] + ', note: ' + f['note']
-
-        elif entry.type == 'book':
-            return authors + ', ' + f['year'] + ', *' + f['title'] + '*, ' + f['publisher']
-
-        elif entry.type == 'techreport':
-            return authors + ', ' + f['year'] + ', *' + f['title'] + '*, ' + f['institution']
-
+        authors,entry = self.prepareSingleEntry(key)
+        if authors is None:
+            return None
         else:
-            print(entry)
+            year        = entry.fields['year']
+            title       = entry.fields['title']
+            journal     = entry.fields['journal']
+            volume      = entry.fields['volume']
+            pages       = entry.fields['pages']
+            booktitle   = entry.fields['booktitle']
+            school      = entry.fields['school']
+            note        = entry.fields['note']
+            publisher   = entry.fields['publisher']
+            institution = entry.fields['institution']
 
-    def entry2HTML(self, key):
+            if entry.type == 'article':
+                return authors + ', ' + year + ', ' + title + ', *' + journal + '*, ' + volume + ':' + pages
+            elif entry.type == 'conference':
+                return authors + ', ' + year + ', ' + title + ', *' + booktitle + '*'
+            elif entry.type == 'mastersthesis' or entry.type == 'phdthesis':
+                return authors + ', ' + year + ', ' + title + ', *' + school + '*'
+            elif entry.type == 'unpublished':
+                return authors + ', ' + year + ', ' + title + ', note: ' + note
+            elif entry.type == 'book':
+                return authors + ', ' + year + ', *' + title + '*, ' + publisher
+            elif entry.type == 'techreport':
+                return authors + ', ' + year + ', *' + title + '*, ' + institution
+            else:
+                print(u"There was an error processing: {0}".format(entry))
+                return None
 
-        if key.startswith('__'):
-            return ''
 
-        entry = self.getEntry(key)
+    def entry2html(self, key):
 
-        if entry is None:
-            return ''
-
-        try:
-            authors = '; '.join([self.accent_substitutions(unicode(author).decode('ascii').encode('utf-8')) for author in entry.persons['author']])
-        except UnicodeEncodeError:
-            print('Decoding error for',[author for author in entry.persons['author']])
-
-        if authors.find('{') > -1 or authors.find('}') > -1:
-            print(authors)
-            raise ValueError("authors [{authors:s}] may not have '{{' or '}}' character".format(authors = authors))
-
-        fields = entry.fields
-
-        # Strip off the opening and closing brackets
-        fields['title'] = fields['title'].strip()
-        if fields['title'].startswith('{') and fields['title'].endswith('}'):
-            fields['title'] = fields['title'][1:len(entry.fields['title'])-1]
-
-        f = fields
-        for key in f:
-            f[key] = self.DE(f[key])
-        authors = str(authors)
-
-        if entry.type == 'article':
-            if 'journal' not in fields: fields['journal'] = ''
-            if 'volume' not in fields: fields['volume'] = ''
-            if 'pages' not in fields: fields['pages'] = ''
-
-            return authors + ', ' + f['year'] + ', ' + f['title'] + ', <i>' + f['journal'] + '</i>, ' + f['volume'] + ':' + f['pages']
-
-        elif entry.type == 'conference':
-            if 'journal' not in f: f['journal'] = ''
-            return authors + ', ' + f['year'] + ', ' + f['title'] + ', <i>' + f['booktitle'] + '</i>'
-
-        elif entry.type == 'mastersthesis':
-            return authors + ', ' + f['year'] + ', ' + f['title'] + ', <i>' + f['school'] + '</i>'
-
-        elif entry.type == 'unpublished':
-            return authors + ', ' + f['year'] + ', ' + f['title'] + ', note: ' + f['note']
-
-        elif entry.type == 'book':
-            return authors + ', ' + f['year'] + ', <i>' + f['title'] + '</i>, ' + f['publisher']
-
-        elif entry.type == 'techreport':
-            return authors + ', ' + f['year'] + ', <i>' + f['title'] + '</i>, ' + f['institution']
-
+        authors,entry = self.prepareSingleEntry(key)
+        if authors is None:
+            return None
         else:
-            print(entry)
+            year        = entry.fields['year']
+            title       = entry.fields['title']
+            journal     = entry.fields['journal']
+            volume      = entry.fields['volume']
+            pages       = entry.fields['pages']
+            booktitle   = entry.fields['booktitle']
+            school      = entry.fields['school']
+            note        = entry.fields['note']
+            publisher   = entry.fields['publisher']
+            institution = entry.fields['institution']
+
+            if entry.type == 'article':
+                return authors + ', ' + year + ', ' + title + ', <i>' + journal + '</i>, ' + volume + ':' + pages
+            elif entry.type == 'conference':
+                return authors + ', ' + year + ', ' + title + ', <i>' + booktitle + '</i>'
+            elif entry.type == 'mastersthesis' or entry.type == 'phdthesis':
+                return authors + ', ' + year + ', ' + title + ', <i>' + school + '</i>'
+            elif entry.type == 'unpublished':
+                return authors + ', ' + year + ', ' + title + ', note: ' + note
+            elif entry.type == 'book':
+                return authors + ', ' + year + ', <i>' + title + '</i>, ' + publisher
+            elif entry.type == 'techreport':
+                return authors + ', ' + year + ', <i>' + title + '</i>, ' + institution
+            else:
+                print(u"There was an error processing: {0}".format(entry))
+                return None
 
 
     def entries2All(self,
-        keys=[],
+
+        keys=None,
         output_encoding=None,
         output_backend='plaintext',
         style='unsrt'):
@@ -216,7 +258,7 @@ class BibTeXerClass(object):
         style_cls = find_plugin('pybtex.style.formatting', style)
         style = style_cls()
         bib_data = self.library #[ self.getEntry(key) for key in keys ]
-        formatted_bibliography = style.format_bibliography(bib_data)
+        formatted_bibliography = style.format_bibliography(bib_data,citations=keys)
 
         output_backend = find_plugin('pybtex.backends', output_backend)
         #output_filename = filename + output_backend.default_suffix
@@ -229,13 +271,17 @@ class BibTeXerClass(object):
         # Close object and discard memory buffer --
         # .getvalue() will now raise an exception.
         stream.close()
-        print(contents)
+        return contents
+
+
 
 if __name__=='__main__':
-    B = BibTeXerClass()
-    B.loadLibrary('../../../Web/fluid_properties/Incompressibles.bib')
-    print(B.entry2rst('Cesar2013'))
-    print(B.entries2All(keys=['Cesar2013']))
-#     B = BibTeXerClass(fName='../../../Web/CoolPropBibTeXLibrary.bib')
-#     print(B.entry2rst('Mulero-JPCRD-2012'))
-#     print(B.entries2All(keys=['Mulero-JPCRD-2012']))
+#     B = BibTeXerClass()
+#     B.loadLibrary('../../../Web/fluid_properties/Incompressibles.bib')
+#     print(B.entry2rst('Cesar2013'))
+#     print(B.entry2html('Cesar2013'))
+#     print(B.entries2All(keys=['Cesar2013']))
+    B = BibTeXerClass('../../../CoolPropBibTeXLibrary.bib')
+    print(B.entry2rst('Mulero-JPCRD-2012'))
+    print(B.entry2html('Mulero-JPCRD-2012'))
+    print(B.entries2All(keys=['Mulero-JPCRD-2012']))
