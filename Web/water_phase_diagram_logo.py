4b806195a80a7404b70ed042219cc211ec960493
Ian Bell <ian.h.bell@gmail.com>
2014-10-07 23:19:08 +0200

    Updated logo script
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

--- Web/water_phase_diagram_logo.py
+++ Web/water_phase_diagram_logo.py
@@ -3,6 +3,7 @@ matplotlib.use('TkAgg')
 import numpy as np
 import CoolProp as CP
 import matplotlib.pyplot as plt
+import scipy.interpolate
 
 Water = CP.AbstractState("HEOS", "Water")
 pc = Water.keyed_output(CP.iP_critical)
@@ -15,9 +16,9 @@ Tt = 273.16
 fillcolor = 'g'
 
 fig = plt.figure(figsize = (3,3))
-ax = fig.add_axes((0.02,0.02,0.98,0.98))
-lw = 4
-melt_args = dict(lw = lw)
+ax = fig.add_axes((0.02,0.00,0.98,1))
+lw = 3
+melt_args = dict(lw = lw, solid_capstyle = 'round')
 TT = []
 PP = list(np.logspace(np.log10(pt), np.log10(pmax),1000))
 for p in PP:
@@ -31,45 +32,28 @@ for T in np.linspace(max(TT), 355):
     p = pi*632.4e6
     PP.append(p)
 
-#Zone VII
-for T in np.linspace(355, 715):
-    TT.append(T)
-    theta = T/355
-    lnpi = 0.173683e1*(1-1/theta)-0.544606e-1*(1-theta**5)+0.806106e-7*(1-theta**22)
-    p = np.exp(lnpi)*2216e6
-    PP.append(p)
-plt.plot(TT,PP,'g',**melt_args)
-
-# Supercritical liquid
-#plt.fill_betweenx(PP,TT,Tc,color=fillcolor,alpha = 0.9, where =  np.logical_and(np.array(PP)>pc, np.array(TT)<Tc) )
+#~ #Zone VII
+#~ for T in np.linspace(355, 715):
+    #~ TT.append(T)
+    #~ theta = T/355
+    #~ lnpi = 0.173683e1*(1-1/theta)-0.544606e-1*(1-theta**5)+0.806106e-7*(1-theta**22)
+    #~ p = np.exp(lnpi)*2216e6
+    #~ PP.append(p)
+plt.plot(TT,PP,'darkblue',**melt_args)
 
 Ts = np.linspace(273.16, Tc, 1000)
 ps = CP.CoolProp.PropsSI('P','T',Ts,'Q',[0]*len(Ts),'Water',[1])
 
-#~ TT = []
-#~ PP = np.logspace(np.log10(pt), np.log10(pc), 1000)
-#~ for p in PP:
-    #~ TT.append(Water.melting_line(CP.iT, CP.iP, p))
-#~ plt.plot(TT,PP,'g',**melt_args)
-
-# Liquid
-#plt.fill(np.r_[TT,Ts[::-1]],np.r_[PP,ps[::-1]],fillcolor, alpha = 0.7)
-# Gas
-#plt.fill(np.r_[Ts,Tc,Ts[0]],np.r_[ps,pt,ps[0]],fillcolor, alpha = 0.1)
-# Supercritical gas
-#plt.fill(np.r_[Tc,Tmax, Tmax, Tc, Tc],np.r_[pc, pc, pt, pt, pc],fillcolor, alpha = 0.3)
 # Supercritical
 pe = 1.3e10
-#plt.fill(np.r_[Tc, Tc, Tmax, Tmax],np.r_[pc, pe, pe, pc],fillcolor, alpha = 0.5)
 
-plt.plot(Ts,ps,'k',lw = lw)
-plt.plot(Ts,ps,'w',lw = lw/2)
-
-plt.plot(Tc,pc,'ro')
+plt.plot(Ts,ps,'orange',lw = lw, solid_capstyle = 'round')
 
 TD,DD,PD = [], [], []
-for T in np.linspace(310,1000,200):
-    for p in np.logspace(np.log10(1000),np.log10(1e9),200):
+for T in np.linspace(240,1000,200):
+    for p in np.logspace(np.log10(612),np.log10(1e9),200):
+        Tm = scipy.interpolate.interp1d(PP,TT)(p)
+        if T < Tm: continue
         D = CP.CoolProp.PropsSI('D','T',T,'P',p,'Water')
         TD.append(T)
         DD.append(np.log10(D))
@@ -78,7 +62,7 @@ for T in np.linspace(310,1000,200):
 nm = matplotlib.colors.Normalize(min(DD), max(DD))
 plt.scatter(TD, PD, c = DD, edgecolor = 'none', s = 6, cmap=plt.cm.get_cmap('Blues'), norm = nm)
 
-plt.ylim(611,max(PP)*1.2)
+plt.ylim(611,1e9)
 plt.gca().set_yscale('log')    
 #plt.gca().set_xlim(Tmin*0.99, Tmax)
 plt.gca().set_xlim(240, 1000)
