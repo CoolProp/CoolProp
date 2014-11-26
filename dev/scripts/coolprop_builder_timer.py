37dabf1c98407602edda25dbb9c24c17bd84f19a
Ian Bell <ian.h.bell@gmail.com>
2014-11-26 13:32:50 -0500

    Add timer to builders
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

---
+++ dev/scripts/coolprop_builder_timer.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+
+import urllib, json
+filehandle = urllib.urlopen('http://www.coolprop.dreamhosters.com:8010/json/builders')
+jj = json.loads(filehandle.read())
+
+times = []
+for key in jj.keys():
+    filehandle = urllib.urlopen('http://www.coolprop.dreamhosters.com:8010/json/builders/' + key + '/builds/-1')
+    builder = json.loads(filehandle.read())
+    elapsed_time = builder['times'][1] - builder['times'][0]
+    times.append((elapsed_time, key))
+    
+print(sorted(times)[::-1])
