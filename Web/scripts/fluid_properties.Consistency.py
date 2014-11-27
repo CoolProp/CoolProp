89ef2dc7721f4730b86ee06ae048c07c2c40bc1c
Ian Bell <ian.h.bell@gmail.com>
2014-11-27 17:58:04 -0500

    Added consistency generator
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

---
+++ Web/scripts/fluid_properties.Consistency.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+import os.path
+import CoolProp
+import subprocess
+import sys
+
+web_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+root_dir = os.path.abspath(os.path.join(web_dir, '..')) 
+fluids_path = os.path.join(web_dir,'fluid_properties','fluids')
+plots_path = os.path.join(web_dir,'fluid_properties','fluids','Consistencyplots')
+
+template = """from __future__ import division, print_function
+import matplotlib
+matplotlib.use('Agg') #Force mpl to use a non-GUI backend
+
+import matplotlib.pyplot as plt
+from CoolProp.Plots.ConsistencyPlots import ConsistencyFigure
+
+ff = ConsistencyFigure('{fluid:s}')
+ff.savefig('{fluid:s}.png', dpi = 30)
+ff.savefig('{fluid:s}.pdf')
+plt.close()
+del ff
+"""
+if not os.path.exists(plots_path):
+    os.makedirs(plots_path)
+    
+for fluid in CoolProp.__fluids__:
+    print('fluid:', fluid)
+    file_string = template.format(fluid = fluid)
+    file_path = os.path.join(plots_path, fluid + '.py')
+    print('Writing to', file_path)
+    with open(file_path, 'w') as fp:
+        fp.write(file_string)
+    print('calling:', 'python "' + fluid + '.py"', 'in',plots_path)
+    subprocess.check_call('python "' + fluid + '.py"', cwd = plots_path, stdout = sys.stdout, stderr = sys.stderr, shell = True)
