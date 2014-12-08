e62d11263c9d8792f0a8acf4d4aede22a3c35b50
Jorrit Wronski <jowr@mek.dtu.dk>
2014-12-08 23:32:18 +0100

    Update buildbot.sh
    Fixes issue #300 - buildbot script exits with 1 on git pull failure.

--- dev/scripts/buildbot.sh
+++ dev/scripts/buildbot.sh
@@ -5,42 +5,42 @@ source /home/$USER/.bash_profile
 source /home/$USER/buildbot/server-master-sandbox/bin/activate
 #
 function start {
-  buildbot start /home/$USER/buildbot/server-master/
+    buildbot start /home/$USER/buildbot/server-master/
 }
 function reconfig {
-  buildbot reconfig /home/$USER/buildbot/server-master/
+    buildbot reconfig /home/$USER/buildbot/server-master/
 }
 function git_pull {
-  pushd /home/$USER/buildbot/CoolProp.git
-  git pull
-  popd
+    pushd /home/$USER/buildbot/CoolProp.git
+    if git pull; then
+        echo "Updated Git repository"
+    else
+        echo "\"git pull\" failed, aborting."
+        exit 1
+    fi
+    popd
 }
 function stop {
-#  PID=$(ps aux | grep -m 1 'python=buildbot.tac' | tr -s " " | cut -d " " -s -f 2)
-#  if [ "$PID" -eq "$PID" ] 2>/dev/null; then
-#    echo "Killing $PID"
-#    kill $PID
-#  fi
-  buildbot stop /home/$USER/buildbot/server-master/
+    buildbot stop /home/$USER/buildbot/server-master/
 }
 #
 # Check for input
 CMD="$1"
 if [ "$CMD" = "restart" ]; then
-  stop
-  git_pull
-  start
+    stop
+    git_pull
+    start
 elif [ "$CMD" = "reconfig" ]; then
-  git_pull
-  reconfig
+    git_pull
+    reconfig
 elif [ "$CMD" = "start" ]; then
-  git_pull
-  start
+    git_pull
+    start
 elif [ "$CMD" = "stop" ]; then
-  stop
+    stop
 else
-  git_pull
-  start
+    git_pull
+    start
 fi
 #
 echo "Script executed, terminating"
