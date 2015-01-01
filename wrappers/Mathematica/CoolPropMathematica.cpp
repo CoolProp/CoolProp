5fe46a0a1e3cc7fd873fccc33078c4389cd62cf0
Ian Bell <ian.h.bell@gmail.com>
2015-01-01 05:07:01 +0100

    Fixed mathematica wrapper for windows
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

--- wrappers/Mathematica/CoolPropMathematica.cpp
+++ wrappers/Mathematica/CoolPropMathematica.cpp
@@ -1,6 +1,7 @@
 
 /* Include required header */
-#include "CoolPropLib.h"
+#include "CoolProp.h"
+#include "HumidAirProp.h"
 #include "WolframLibrary.h"
 
 /* Return the version of Library Link */
@@ -26,21 +27,6 @@ extern "C" DLLEXPORT int plus_one( WolframLibraryData libData, mint Argc, MArgum
 	return LIBRARY_NO_ERROR;
 }
 
-extern "C" DLLEXPORT int Props( WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
-    if (Argc != 6){ return LIBRARY_FUNCTION_ERROR; }
-    char *outstring = MArgument_getUTF8String(Args[0]);
-    char *In1string = MArgument_getUTF8String(Args[1]);
-    mreal In1val = MArgument_getReal(Args[2]);
-    char *In2string = MArgument_getUTF8String(Args[3]);
-    mreal In2val = MArgument_getReal(Args[4]);
-    char *Fluidstring = MArgument_getUTF8String(Args[5]);
-    
-    // PropsS version takes all strings, not single-character inputs
-	double val = PropsS(outstring,In1string,(double)In1val,In2string,(double)In2val,Fluidstring);
-	MArgument_setReal(Res, val);
-	return LIBRARY_NO_ERROR;
-}
-
 extern "C" DLLEXPORT int PropsSI( WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
     if (Argc != 6){ return LIBRARY_FUNCTION_ERROR; }
     char *outstring = MArgument_getUTF8String(Args[0]);
@@ -51,12 +37,12 @@ extern "C" DLLEXPORT int PropsSI( WolframLibraryData libData, mint Argc, MArgume
     char *Fluidstring = MArgument_getUTF8String(Args[5]);
     
     // PropsS version takes all strings, not single-character inputs
-	double val = PropsSI(outstring,In1string,(double)In1val,In2string,(double)In2val,Fluidstring);
+	double val = CoolProp::PropsSI(outstring,In1string,(double)In1val,In2string,(double)In2val,Fluidstring);
 	MArgument_setReal(Res, val);
 	return LIBRARY_NO_ERROR;
 }
 
-extern "C" DLLEXPORT int HumidAirProps( WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
+extern "C" DLLEXPORT int HAPropsSI( WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Res) {
     if (Argc != 7){ return LIBRARY_FUNCTION_ERROR; }
     char *outstring = MArgument_getUTF8String(Args[0]);
     char *In1string = MArgument_getUTF8String(Args[1]);
@@ -66,7 +52,7 @@ extern "C" DLLEXPORT int HumidAirProps( WolframLibraryData libData, mint Argc, M
     char *In3string = MArgument_getUTF8String(Args[5]);
     mreal In3val = MArgument_getReal(Args[6]);
     
-	double val = HAProps(outstring,In1string,(double)In1val,In2string,(double)In2val,In3string,(double)In3val);
+	double val = HumidAir::HAPropsSI(outstring,In1string,(double)In1val,In2string,(double)In2val,In3string,(double)In3val);
 	MArgument_setReal(Res, val);
 	return LIBRARY_NO_ERROR;
 }
