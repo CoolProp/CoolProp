7c108c9ac28635d655ee4515ea6702843776fa5e
Ian Bell <ian.h.bell@gmail.com>
2015-01-14 22:24:10 -0700

    Added VB.net solution files
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

---
+++ wrappers/VB.NET/CoolPropVB/CoolPropVB/Example.vb
@@ -0,0 +1,19 @@
+﻿Module Example
+
+    Sub Main()
+        ' High-level interface example
+        Dim Tbp As Double
+        Tbp = CoolProp.CoolProp.PropsSI("T", "P", 101325, "Q", 0, "Water")
+        Console.Write(Tbp)
+        Console.Write(vbCrLf)
+
+        ' Low-level interface example
+        Dim Water As CoolProp.AbstractState
+        Water = CoolProp.AbstractState.factory("HEOS", "Water")
+        Water.update(CoolProp.input_pairs.PQ_INPUTS, 101325, 0)
+        Console.Write(Water.T())
+        Console.Write(vbCrLf)
+
+    End Sub
+
+End Module
