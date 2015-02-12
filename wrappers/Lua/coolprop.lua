592c65b5061c9383ba3b46ab1c73a0ec6f78252b
Hisham Muhammad <hisham@gobolinux.org>
2015-02-11 23:21:15 -0200

    Fix and cleanup APIs between Lua and C.
    Fix Lua layer of get_param_index and get_fluid_param_string.
    Make returns of C module more idiomatic.
    Use module name as the local variable for the C module.

--- wrappers/Lua/coolprop.lua
+++ wrappers/Lua/coolprop.lua
@@ -8,99 +8,130 @@ if jit and jit.status() then
     -- If loading via FFI fails, try Lua/C API,
     -- which might contain a statically-linked CoolProp.
 end
-local huge = math.huge
-local ok, lib = pcall(require, "coolprop.capi")
+
+local ok, capi = pcall(require, "coolprop.capi")
 assert(ok, "Unable to load Lua CoolProp C API Wrapper.")
+
+local huge = math.huge
+
 local coolprop = {}
+
 function coolprop.Props1SI(fluidname, output)
-    local v = lib.Props1SI(fluidname, output)
+    local v = capi.Props1SI(fluidname, output)
     if v == huge then
         return nil, coolprop.error()
     end
     return v
 end
+
 function coolprop.PropsSI(output, name1, prop1, name2, prop2, ref)
-    local v = lib.PropsSI(output, name1, prop1, name2, prop2, ref)
+    local v = capi.PropsSI(output, name1, prop1, name2, prop2, ref)
     if v == huge then
         return nil, coolprop.error()
     end
     return v
 end
+
 function coolprop.PhaseSI(name1, prop1, name2, prop2, ref)
-    local e, s = lib.PhaseSI(name1, prop1, name2, prop2, ref or "")
-    return e == 1 and s or nil
+    local phase, err = capi.PhaseSI(name1, prop1, name2, prop2, ref or "")
+    if not phase then
+        return nil, err or coolprop.error()
+    end
+    return phase
 end
+
 function coolprop.get_global_param_string(param)
-    local e, s = lib.get_global_param_string(param)
-    if e == 1 then
-        return s
+    local v, err = capi.get_global_param_string(param)
+    if not v then
+        return nil, err or coolprop.error()
     end
-    return nil
+    return v
 end
+
 function coolprop.get_parameter_information_string(key)
-    local e, s = lib.get_parameter_information_string(key)
-    if e == 0 then return nil end
-    return s
+    local val, err = capi.get_parameter_information_string(key)
+    if not val then
+        return nil, err or coolprop.error()
+    end
+    return val
 end
+
 function coolprop.get_mixture_binary_pair_data(cas1, cas2, key)
-    return tonumber(lib.get_mixture_binary_pair_data(cas1, cas2, key))
+    return tonumber(capi.get_mixture_binary_pair_data(cas1, cas2, key))
 end
+
 function coolprop.get_fluid_param_string(fluid, param)
-    if lib.get_fluid_param_string(fluid, param, bub, 4096) == 1 then
-        return ffi_str(bub)
+    local val, err = capi.get_fluid_param_string(fluid, param)
+    if not val then
+        return nil, err or coolprop.error()
     end
-    return nil
+    return val
 end
+
 function coolprop.set_reference_stateS(ref, state)
-    return lib.set_reference_stateS(ref, state) == 1
+    return capi.set_reference_stateS(ref, state)
 end
+
 function coolprop.set_reference_stateD(ref, t, rho, h0, s0)
-    return lib.set_reference_stateD(ref, t, rho, h0, s0) == 1
+    return capi.set_reference_stateD(ref, t, rho, h0, s0)
 end
+
 function coolprop.F2K(f)
-    return lib.F2K(f)
+    return capi.F2K(f)
 end
+
 function coolprop.K2F(k)
-    return lib.K2F(k)
+    return capi.K2F(k)
 end
+
 function coolprop.get_param_index(param)
-    local value = lib.get_param_index(param)
+    local value = capi.get_param_index(param)
     if value == -1 then return nil end
-    tonumber(value)
+    return tonumber(value)
 end
+
 function coolprop.saturation_ancillary(fluid, output, q, input, value)
-    local v = lib.saturation_ancillary(fluid, output, q, input, value)
+    local v = capi.saturation_ancillary(fluid, output, q, input, value)
     if v == huge then
         return nil, coolprop.error()
     end
     return v
 end
+
 function coolprop.redirect_stdout(file)
-    return lib.redirect_stdout(file) == 1
+    return capi.redirect_stdout(file)
 end
+
 function coolprop.get_debug_level()
-    return lib.get_debug_level()
+    return capi.get_debug_level()
 end
+
 function coolprop.set_debug_level(level)
-    lib.set_debug_level(level)
+    capi.set_debug_level(level)
 end
+
 function coolprop.HAPropsSI(output, name1, prop1, name2, prop2, name3, prop3)
-    local v = lib.HAPropsSI(output, name1, prop1, name2, prop2, name3, prop3)
+    local v = capi.HAPropsSI(output, name1, prop1, name2, prop2, name3, prop3)
     if v == huge then
         return nil, coolprop.error()
     end
     return v
 end
+
 function coolprop.error()
     return coolprop.get_global_param_string("errstring")
 end
+
 function coolprop.FluidsList()
     return coolprop.get_global_param_string("FluidsList")
 end
+
 function coolprop.version()
     return coolprop.get_global_param_string("version")
 end
+
 function coolprop.gitrevision()
     return coolprop.get_global_param_string("gitrevision")
 end
+
 return coolprop
--- wrappers/Lua/coolprop/capi.c
+++ wrappers/Lua/coolprop/capi.c
@@ -4,6 +4,7 @@
 #include <lauxlib.h>
 #include "../../../include/CoolPropLib.h"
 #include "compat.h"
+
 static int lua_coolprop_Props1SI(lua_State *L) {
     lua_pushnumber(L, Props1SI(
         luaL_checkstring(L, 1),
@@ -11,6 +12,7 @@ static int lua_coolprop_Props1SI(lua_State *L) {
     ));
     return 1;
 }
+
 static int lua_coolprop_PropsSI(lua_State *L) {
     lua_pushnumber(L, PropsSI(
         luaL_checkstring(L, 1),
@@ -22,9 +24,10 @@ static int lua_coolprop_PropsSI(lua_State *L) {
     ));
     return 1;
 }
+
 static int lua_coolprop_PhaseSI(lua_State *L) {
     char phase[256];
-    lua_pushinteger(L, PhaseSI(
+    int ok = PhaseSI(
         luaL_checkstring(L, 1),
         luaL_checknumber(L, 2),
         luaL_checkstring(L, 3),
@@ -32,30 +35,43 @@ static int lua_coolprop_PhaseSI(lua_State *L) {
         luaL_checkstring(L, 5),
         phase,
         sizeof(phase)
-    ));
-    lua_pushstring(L, phase);
-    return 2;
+    );
+    if (ok == 1) {
+        lua_pushstring(L, phase);
+        return 1;
+    } else {
+        return 0;
+    }
 }
+
 static int lua_coolprop_get_global_param_string(lua_State *L) {
     char output[4096];
-    lua_pushinteger(L, get_global_param_string(
+    int ok = get_global_param_string(
         luaL_checkstring(L, 1),
         output,
         sizeof(output)
-    ));
-    lua_pushstring(L, output);
-    return 2;
+    );
+    if (ok == 1) {
+        lua_pushstring(L, output);
+        return 1;
+    }
+    return 0;
 }
+
 static int lua_coolprop_get_parameter_information_string(lua_State *L) {
     char output[4096];
-    lua_pushinteger(L, get_parameter_information_string(
+    int ok = get_parameter_information_string(
         luaL_checkstring(L, 1),
         output,
         sizeof(output)
-    ));
-    lua_pushstring(L, output);
-    return 2;
+    );
+    if (ok == 1) {
+        lua_pushstring(L, output);
+        return 1;
+    }
+    return 0;
 }
+
 /*
 static int lua_coolprop_get_mixture_binary_pair_data(lua_State *L) {
     lua_pushinteger(L, get_mixture_binary_pair_data(
@@ -66,26 +82,32 @@ static int lua_coolprop_get_mixture_binary_pair_data(lua_State *L) {
     return 1;
 }
 */
+
 static int lua_coolprop_get_fluid_param_string(lua_State *L) {
     char output[4096];
-    lua_pushinteger(L, get_fluid_param_string(
+    int ok = get_fluid_param_string(
         luaL_checkstring(L, 1),
         luaL_checkstring(L, 2),
         output,
         sizeof(output)
-    ));
-    lua_pushstring(L, output);
-    return 2;
+    );
+    if (ok == 1) {
+        lua_pushstring(L, output);
+        return 1;
+    }
+    return 0;
 }
+
 static int lua_coolprop_set_reference_stateS(lua_State *L) {
-    lua_pushinteger(L, set_reference_stateS(
+    lua_pushboolean(L, set_reference_stateS(
         luaL_checkstring(L, 1),
         luaL_checkstring(L, 2)
     ));
     return 1;
 }
+
 static int lua_coolprop_set_reference_stateD(lua_State *L) {
-    lua_pushinteger(L, set_reference_stateD(
+    lua_pushboolean(L, set_reference_stateD(
         luaL_checkstring(L, 1),
         luaL_checknumber(L, 2),
         luaL_checknumber(L, 3),
@@ -94,32 +116,39 @@ static int lua_coolprop_set_reference_stateD(lua_State *L) {
     ));
     return 1;
 }
+
 static int lua_coolprop_F2K(lua_State *L) {
     lua_pushnumber(L, F2K(luaL_checknumber(L, 1)));
     return 1;
 }
+
 static int lua_coolprop_K2F(lua_State *L) {
     lua_pushnumber(L, K2F(luaL_checknumber(L, 1)));
     return 1;
 }
+
 static int lua_coolprop_get_param_index(lua_State *L) {
     lua_pushnumber(L, get_param_index(luaL_checkstring(L, 1)));
     return 1;
 }
+
 static int lua_coolprop_redirect_stdout(lua_State *L) {
-    lua_pushnumber(L, redirect_stdout(
+    lua_pushboolean(L, redirect_stdout(
         luaL_checkstring(L, 1)
     ));
     return 1;
 }
+
 static int lua_coolprop_get_debug_level(lua_State *L) {
     lua_pushinteger(L, get_debug_level());
     return 1;
 }
+
 static int lua_coolprop_set_debug_level(lua_State *L) {
     set_debug_level(luaL_checknumber(L, 1));
     return 0;
 }
+
 static int lua_coolprop_saturation_ancillary(lua_State *L) {
     lua_pushnumber(L, saturation_ancillary(
         luaL_checkstring(L, 1),
@@ -130,6 +159,7 @@ static int lua_coolprop_saturation_ancillary(lua_State *L) {
     ));
     return 1;
 }
+
 static int lua_coolprop_HAPropsSI(lua_State *L) {
     lua_pushnumber(L, HAPropsSI(
         luaL_checkstring(L, 1),
@@ -142,6 +172,7 @@ static int lua_coolprop_HAPropsSI(lua_State *L) {
     ));
     return 1;
 }
+
 static struct luaL_Reg funcs[] = {
   {"Props1SI", lua_coolprop_Props1SI},
   {"PropsSI", lua_coolprop_PropsSI},
@@ -162,6 +193,7 @@ static struct luaL_Reg funcs[] = {
   {"HAPropsSI", lua_coolprop_HAPropsSI},
   {NULL, NULL}
 };
+
 int luaopen_coolprop_capi(lua_State *L) {
   luaL_newlib(L, funcs);
   return 1;
