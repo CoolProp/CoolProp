ae0468cfae2fc168cff68227819b9d025a05addb
Ian Bell <ian.h.bell@gmail.com>
2015-03-13 19:32:15 -0600

    Removed old-school examples, implemented auto-generated examples in CMake
    Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

--- Web/coolprop/examples/MATLAB/run_example.bat
+++
@@ -1,7 +0,0 @@
-copy ..\..\..\wrappers\MATLAB\example.m .
-copy ..\..\..\wrappers\MATLAB\*rops.c .
-matlab -wait -nojvm -nodesktop -r "MATLABBuilder; quit"
-cl /c /I../../../CoolProp /EHsc CoolProp_wrap.cxx
-matlab -nojvm -nodesktop -r "Example; quit" -logfile Output.txt
-cl /c /I../../../CoolProp /EHsc CoolProp_wrap.cxx
-erase *Props*.c
