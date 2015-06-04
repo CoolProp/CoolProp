a1ece4967f0030d41ceb69a9d6beda51e6b370e0
Ian Bell <ian.h.bell@gmail.com>
2015-06-03 21:37:05 -0600

    Added code for adjugate

--- include/CoolPropTools.h
+++ include/CoolPropTools.h
@@ -20,7 +20,8 @@
     #include <numeric>
     #include <set>
     #include <sys/types.h>
-    #include <sys/stat.h>    
+    #include <sys/stat.h>   
+    #include <Eigen/Core>
 
     // This will kill the horrible min and max macros 
     #ifndef NOMINMAX
@@ -264,6 +265,46 @@
             return(contents);
         }
         throw(errno);
+    }
+    inline void removeRow(Eigen::MatrixXd& matrix, unsigned int rowToRemove)
+    {
+        unsigned int numRows = matrix.rows()-1;
+        unsigned int numCols = matrix.cols();
+
+        if( rowToRemove < numRows )
+            matrix.block(rowToRemove,0,numRows-rowToRemove,numCols) = matrix.block(rowToRemove+1,0,numRows-rowToRemove,numCols);
+
+        matrix.conservativeResize(numRows,numCols);
+    };
+    inline void removeColumn(Eigen::MatrixXd& matrix, unsigned int colToRemove)
+    {
+        unsigned int numRows = matrix.rows();
+        unsigned int numCols = matrix.cols()-1;
+
+        if( colToRemove < numCols )
+            matrix.block(0,colToRemove,numRows,numCols-colToRemove) = matrix.block(0,colToRemove+1,numRows,numCols-colToRemove);
+
+        matrix.conservativeResize(numRows,numCols);
+    };
+    template <typename Derived>
+    inline Eigen::MatrixXd minor_matrix(const Eigen::MatrixBase<Derived>& A, std::size_t i, std::size_t j)
+    {
+        Eigen::MatrixXd Am = A;
+        removeRow(Am, i);
+        removeColumn(Am, j);
+        return Am;
+    };
+
+    template <typename Derived>
+    static Eigen::MatrixXd adjugate(const Eigen::MatrixBase<Derived>& A, std::size_t N)
+    {
+        Eigen::MatrixXd Aadj(A.rows(), A.cols());
+        for (std::size_t i = 0; i < N; ++i){
+            for (std::size_t j = 0; j < N; ++j){
+                Aadj(i,j) = pow(-1, i+j)*minor_matrix(A, i, j).determinant();
+            }
+        }
+        return Aadj;
     }
 
     // Missing string printf
