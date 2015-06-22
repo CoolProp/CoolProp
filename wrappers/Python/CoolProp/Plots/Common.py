ea30ad03204e0bb25dab1a303daed5884dd6ceaf
Jorrit Wronski <jowr@ipu.dk>
2015-06-22 20:30:15 +0200

    Started to work on the new plotting functions

--- wrappers/Python/CoolProp/Plots/Common.py
+++ wrappers/Python/CoolProp/Plots/Common.py
@@ -6,10 +6,151 @@ import matplotlib
 import numpy
 
 import CoolProp.CoolProp as CP
+from abc import ABCMeta
+
+
+class BaseQuantity(object):
+    """A very basic property that can convert an input to and from a 
+    given unit system, note that the conversion from SI units starts 
+    with a multiplication. If you need to remove an offset, use the 
+    off_SI property. 
+    Examples with temperature:
+    celsius = BaseQuantity(add_SI=-273.15)
+    fahrenheit = BaseQuantity(add_SI=32.0, mul_SI=1.8, off_SI=-273.15)
+    Examples with pressure:
+    bar = BaseQuantity(mul_SI=1e-5)
+    psi = BaseQuantity(mul_SI=0.000145037738)    
+    """
+    def __init__(self, add_SI=0.0, mul_SI=1.0, off_SI=0.0):
+        self._add_SI = add_SI
+        self._mul_SI = mul_SI
+        self._off_SI = off_SI
+
+    @property
+    def add_SI(self): return self._add_SI
+    @add_SI.setter
+    def add_SI(self, value): self._add_SI = value
+    @property
+    def mul_SI(self): return self._mul_SI
+    @mul_SI.setter
+    def mul_SI(self, value): self._mul_SI = value
+    @property
+    def off_SI(self): return self._off_SI
+    @off_SI.setter
+    def off_SI(self, value): self._off_SI = value
+    
+    def from_SI(self, value): return ((value+self.off_SI)*self.mul_SI)+self.add_SI
+    def to_SI(self, value): return (value-self.add_SI)/self.mul_SI-self.off_SI
+    
+
+class BaseDimension(BaseQuantity):
+    """A dimension is a class that extends the BaseQuantity and adds a label, a symbol and a unit label"""
+    def __init__(self, add_SI=0.0, mul_SI=1.0, off_SI=0.0, label='', symbol='', unit=''):
+        self._label = label
+        self._symbol = symbol
+        self._unit = unit
+        super(BaseDimension, self).__init__(add_SI=add_SI, mul_SI=mul_SI, off_SI=off_SI)
+
+    @property
+    def label(self): return self._label
+    @label.setter
+    def label(self, value): self._label = value
+    @property
+    def symbol(self): return self._symbol
+    @symbol.setter
+    def symbol(self, value): self._symbol = value
+    @property
+    def unit(self): return self._unit
+    @unit.setter
+    def unit(self, value): self._unit = value
+
+
+class UnitSystem(object):
+    """A collection of dimensions for all the required quantities"""
+    __metaclass__ = ABCMeta
+    @property
+    def D(self): return self._D
+    @D.setter
+    def D(self, value): self._D = value
+    @property
+    def H(self): return self._H
+    @H.setter
+    def H(self, value): self._H = value
+    @property
+    def P(self): return self._P
+    @P.setter
+    def P(self, value): self._P = value
+    @property
+    def S(self): return self._S
+    @S.setter
+    def S(self, value): self._S = value
+    @property
+    def T(self): return self._T
+    @T.setter
+    def T(self, value): self._T = value
+    @property
+    def U(self): return self._U
+    @U.setter
+    def U(self, value): self._U = value
+    
+    def dimensions(self): 
+        return {
+      'D' : self._D,
+      'H' : self._H,
+      'P' : self._P,
+      'S' : self._S,
+      'T' : self._T,
+      'U' : self._U
+    }
+
+
+class SIunits(UnitSystem):
+    def __init__(self):
+        self._D = BaseDimension(add_SI=0.0, mul_SI=1.0, off_SI=0.0, label='Density',                  symbol=r'$\rho$', unit=r'kg/m$^3$')
+        self._H = BaseDimension(add_SI=0.0, mul_SI=1.0, off_SI=0.0, label='Specific Enthalpy',        symbol=r'$h$',    unit=r'J/kg')
+        self._P = BaseDimension(add_SI=0.0, mul_SI=1.0, off_SI=0.0, label='Pressure',                 symbol=r'$p$',    unit=r'Pa')
+        self._S = BaseDimension(add_SI=0.0, mul_SI=1.0, off_SI=0.0, label='Specific Entropy',         symbol=r'$s$',    unit=r'J/kg/K')
+        self._T = BaseDimension(add_SI=0.0, mul_SI=1.0, off_SI=0.0, label='Temperature',              symbol=r'$T$',    unit=r'K')
+        self._U = BaseDimension(add_SI=0.0, mul_SI=1.0, off_SI=0.0, label='Specific Internal Energy', symbol=r'$u$',    unit=r'J/kg')
+
+class KSIunits(SIunits):
+    def __init__(self):
+        super(KSIunits, self).__init__()
+        self.H.mul_SI=1e-3
+        self.H.unit=r'kJ/kg'
+        self.P.mul_SI=1e-3
+        self.P.unit=r'kPa'
+        self.S.mul_SI=1e-3
+        self.S.unit=r'kJ/kg/K'
+        self.U.mul_SI=1e-3
+        self.U.unit=r'kJ/kg'
+
+class EURunits(KSIunits):
+    def __init__(self):
+        super(EURunits, self).__init__()
+        self.P.mul_SI=1e-5
+        self.P.unit=r'bar'
+        self.T.add_SI=-273.15
+        self.T.unit=r'C'
+
+
+
+if __name__ == "__main__":
+    for syst in [SIunits(), KSIunits(), EURunits()]:
+        print(syst.P.label)
+        print(syst.P.to_SI(20))
+        print(syst.T.label)
+        print(syst.T.to_SI(20))
+    
+    
+
+
 
 SMALL = 1E-5
 
 class BasePlot(object):
+    
+    
     #TODO: Simplify / Consolidate dictionary maps
     AXIS_LABELS = {'KSI': {'T': ["Temperature", r"[K]"],
                            'P': ["Pressure", r"[kPa]"],
@@ -84,11 +225,20 @@ class BasePlot(object):
         self.graph_drawn = False
         self.fluid_ref = fluid_ref
         self.graph_type = graph_type.upper()
+        
         self.unit_system = unit_system
+#         if unit_system == 'KSI':
+#             self.unit_system = KSIunits()
+#         elif unit_system == 'EUR':
+#             self.unit_system = EURunits()
+#         else:
+#             self.unit_system = SIunits()
+            
 
         self.axis = kwargs.get('axis', None)
         if self.axis is None:
             self.axis = matplotlib.pyplot.gca()
+            
 
     def __sat_bounds(self, kind, smin=None, smax=None):
         """
