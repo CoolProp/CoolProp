51106a5177fe0ec128bb0045ffdf29f5835fff79
Ian Bell <ian.h.bell@gmail.com>
2015-06-28 20:43:14 -0600

    Adding R wrapper code; see #668

---
+++ Web/coolprop/wrappers/R/index.rst
@@ -0,0 +1,83 @@
+.. _R:
+
+************
+R Wrapper
+************
+
+.. contents:: :depth: 2
+
+Pre-compiled Binaries
+=====================
+
+Pre-compiled binaries can be downloaded from :sfdownloads:`R`.  Development binaries coming from the buildbot server can be found at :sfnightly:`R`.  Download the shared library for your platform, as well as the ``CoolProp.R`` module
+
+Usage
+-----
+At the R console, run::
+
+    dyn.load(paste("CoolProp", .Platform$dynlib.ext, sep=""))
+    source("CoolProp.R")
+    cacheMetaData(1)
+    PropsSI("T","P",101325,"Q",0,"Water")
+    
+There is example code :ref:`at the end of this page <r_example>`
+
+User-Compiled Binaries
+======================
+
+Common Requirements
+-------------------
+Compilation of the R wrapper requires a few :ref:`common wrapper pre-requisites <wrapper_common_prereqs>`
+
+Also, you need to install R
+
+Windows
+-------
+
+You must install the MINGW compiler suite.  The `TDM suite <http://tdm-gcc.tdragon.net/>`_ is recommended.
+
+Build
+-----
+
+Linux and OSX
+^^^^^^^^^^^^^
+
+Once the dependencies are installed, you can run the builder and tests using::
+
+    # Check out the sources for CoolProp
+    git clone https://github.com/CoolProp/CoolProp --recursive
+    # Move into the folder you just created
+    mkdir -p  CoolProp/build && cd CoolProp/build
+    # Build the makefile using CMake
+    cmake .. -DCOOLPROP_R_MODULE=ON -DR_BIN="c:\Program Files\R\R-3.2.1\bin\x64" -DCMAKE_BUILD_TYPE=Release
+    # Make the R files
+    make install
+
+Windows (32-bit and 64-bit)
+^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+You need to just slightly modify the building procedure::
+
+    # Check out the sources for CoolProp
+    git clone https://github.com/CoolProp/CoolProp --recursive
+    # Move into the folder you just created
+    cd CoolProp
+    # Make a build folder
+    mkdir build && cd build
+    # Build the makefile using CMake (Must use MINGW since R is built with MINGW)
+    cmake .. -DCOOLPROP_R_MODULE=ON -G "MinGW Makefiles" -DR_BIN="c:\Program Files\R\R-3.2.1\bin\x64" -DCMAKE_BUILD_TYPE=Release
+    # Make the R shared library
+    cmake --build .
+
+.. _r_example:
+
+Example Code
+============
+
+.. literalinclude:: Example.R
+   :language: R
+
+Example Code Output
+===================
+
+.. literalinclude:: Example.out
---
+++ dev/cmake/Modules/FindR.cmake
@@ -0,0 +1,69 @@
+# - Find R
+# This module finds the libraries corresponding to the R program
+# It is based on the code from http://github.com/usnistgov/REFPROP-wrappers which is in the public domain
+
+# This code sets the following variables:
+#
+#  R_LIBRARY             - path to the R shared library
+#  R_INCLUDE_DIRS        - include directory(ies)
+#  R_BIN_DIR             - directory that contains the R executable being used
+
+SET(R_FOUND 0)
+
+IF( "${R_BIN}" STREQUAL "")
+    MESSAGE(FATAL_ERROR "You must pass the CMake variable R_BIN pointing to the directory that contains your executable")
+ENDIF()
+
+find_program(R_EXEC
+             NAMES R
+             PATHS ${R_BIN}
+             )
+execute_process(
+    COMMAND ${R_EXEC} RHOME OUTPUT_VARIABLE R_HOME_TEXT RESULT_VARIABLE R_HOME_RESULT
+)
+MESSAGE(STATUS "R_EXEC= ${R_EXEC}")
+# Parse the output of the R path commmand, removing whitespace
+FUNCTION(chomp arg1 arg2)
+    string(REPLACE "\n" ";" arg1list ${arg1})
+    list(GET arg1list 1 arg1line)
+    string(LENGTH ${arg1line} len)
+    MATH(EXPR length ${len}-6)
+    string(SUBSTRING ${arg1line} 5 ${length} arg1path)
+    # see http://www.cmake.org/pipermail/cmake/2008-November/025423.html
+    set(${arg2} ${arg1path} PARENT_SCOPE)
+ENDFUNCTION(chomp)
+
+MESSAGE(STATUS "R_HOME_TEXT = ${R_HOME_TEXT}")
+MESSAGE(STATUS "R_HOME_RESULT = ${R_HOME_RESULT}")
+file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/get_include.R R.home(component=\"include\"))
+execute_process(
+    COMMAND ${R_EXEC} --quiet -f ${CMAKE_CURRENT_BINARY_DIR}/get_include.R OUTPUT_VARIABLE R_INCLUDE_TEXT RESULT_VARIABLE R_INCLUDE_RESULT
+)
+file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/get_include.R)
+chomp(${R_INCLUDE_TEXT} R_INCLUDE_DIRS)
+MESSAGE(STATUS "R_INCLUDE = ${R_INCLUDE_DIRS} w/ RESULT=${R_INCLUDE_RESULT}")
+
+file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/get_bin.R R.home(component=\"bin\"))
+execute_process(
+    COMMAND ${R_EXEC} --quiet -f ${CMAKE_CURRENT_BINARY_DIR}/get_bin.R OUTPUT_VARIABLE R_BIN_TEXT RESULT_VARIABLE R_BIN_RESULT
+)
+file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/get_bin.R)
+chomp(${R_BIN_TEXT} R_BIN_DIR)
+MESSAGE(STATUS "R_BIN_TEXT = ${R_BIN_DIR} w/ RESULT=${R_BIN_RESULT}")
+        
+find_library (
+          R_LIBRARY
+          R
+          PATHS ${R_BIN_DIR}
+          NO_DEFAULT_PATH
+          NO_CMAKE_PATH
+          )
+MESSAGE(STATUS "R_LIBRARY = ${R_LIBRARY}")
+find_package_handle_standard_args(R DEFAULT_MSG
+                                  R_LIBRARY R_INCLUDE_DIRS)
+
+MARK_AS_ADVANCED(
+  R_BIN_DIR
+  R_LIBRARY
+  R_INCLUDE_DIRS
+)
