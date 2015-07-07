f5463304fa12e92aa98c2e5c67fea89ff666e95d
Ian Bell <ian.h.bell@gmail.com>
2015-07-07 00:33:59 -0600

    Update example runner for R

--- dev/scripts/examples/OSXRun.py
+++ dev/scripts/examples/OSXRun.py
@@ -70,7 +70,7 @@ if __name__=='__main__':
     RR = R()
     RR.write('R/Example.R', RR.parse())
     kwargs = dict(stdout = sys.stdout, stderr = sys.stderr, shell = True, cwd = 'R')
-    subprocess.check_call('cmake ../../../.. -DCOOLPROP_R_MODULE=ON -DCMAKE_VERBOSE_MAKEFILE=ON', **kwargs)
+    subprocess.check_call('cmake ../../../.. -DCOOLPROP_R_MODULE=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DR_BIN=/usr/bin', **kwargs)
     subprocess.check_call('cmake --build .', **kwargs)
     with open('R/Example.out','w') as fp:
         tee_call(r'R Example.R', fp, shell = True, cwd = 'R')
