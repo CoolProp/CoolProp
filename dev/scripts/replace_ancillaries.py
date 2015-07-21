96178b7ec8eeb77723ba820c8ed561c0754a733c
Ian Bell <ian.h.bell@gmail.com>
2015-07-20 20:34:26 -0600

    For pure fluids, only use one saturation pressure curve, pS, instead of pL and pV in json files; closes #711

---
+++ dev/scripts/replace_ancillaries.py
@@ -0,0 +1,27 @@
+#!/Users/ian/anaconda/bin/python
+
+import json, glob, CoolProp
+
+for fluid in glob.glob('../fluids/*.json'):
+	with open(fluid, 'r') as fp:
+		jj = json.load(fp)
+		
+	pL = jj['ANCILLARIES'].pop('pL')
+	pV = jj['ANCILLARIES'].pop('pV')
+	# Keep the one with the lower error
+	if pL['max_abserror_percentage'] < pV['max_abserror_percentage']:
+		pS = pL
+	else:
+		pS = pV
+		
+	pseudo_pure = jj['EOS'][0]['pseudo_pure']
+	if pseudo_pure:
+		print '-----------------PSEUDO (SKIPPING !!!)', fluid
+	else:
+		print fluid
+		jj['ANCILLARIES']['pS'] = pS
+		with open(fluid, 'w') as fp:
+			json.dump(jj, fp)
+
+			
+			
