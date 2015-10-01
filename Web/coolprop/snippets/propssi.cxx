ddec0be08e3fe437fc28d2cf4c59a2e494e105f4
Ian Bell <ian.h.bell@gmail.com>
2015-09-30 21:57:29 -0600

    Fix (and test) all the snippets; closes #823

--- Web/coolprop/snippets/propssi.cxx
+++ Web/coolprop/snippets/propssi.cxx
@@ -12,9 +12,10 @@ int main()
     // Second type (C++ only, a bit faster, allows for vector inputs and outputs)
 	std::vector<std::string> fluids; fluids.push_back("Propane"); fluids.push_back("Ethane");
 	std::vector<std::string> outputs; outputs.push_back("Dmolar");
-	std::vector<double> T(1,298), p(1e5);
-    std::cout << PropsSImulti(outputs,"T", T, "P", p, "", fluids, z)[0][0] << std::endl;
+	std::vector<double> T(1,298), p(1,1e5);
+    std::cout << PropsSImulti(outputs,"T", T, "P", p, "", fluids, z)[0][0] << std::endl; // Default backend is HEOS
     std::cout << PropsSImulti(outputs,"T", T, "P", p, "HEOS", fluids, z)[0][0] << std::endl;
+    // Comment me out if REFPROP is not installed
     std::cout << PropsSImulti(outputs,"T", T, "P", p, "REFPROP", fluids, z)[0][0] << std::endl;
     
     return EXIT_SUCCESS;
