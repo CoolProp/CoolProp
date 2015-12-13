5485c54fc7f8ef144c5aa70933561a112cd98dd0
Bruce Wernick <info@coolit.co.za>
2015-12-13 08:28:06 +0200

    Update uMolChart.pas

--- wrappers/Delphi/uMolChart.pas
+++ wrappers/Delphi/uMolChart.pas
@@ -1,10 +1,14 @@
+{
+  Mollier Chart (ph-chart) generator
+  Bruce Wernick, 13 December 2015
+}
+
 unit uMolChart;
 
 interface
 
 uses
   SysUtils, Windows, Graphics, Classes,
-  superobject,
   cpIntf;
 
 type
@@ -29,7 +33,7 @@ type
     procedure DrawPoint(x, y: Double; Color: TColor);
     procedure DrawLine(x0, y0, x1, y1: Double; Color: TColor; PenWidth: Integer);
     procedure DrawVec(x, y: fvec; Color: TColor; PenWidth: Integer);
-    procedure DrawChart(ref: string; Width, Height: Integer);
+    procedure DrawChart(ref: PAnsiChar; Width, Height: Integer);
   end;
 
 implementation
@@ -40,7 +44,7 @@ uses
 constructor TMolChart.Create(Canvas: TCanvas);
 begin
   FMargin := 25;
-  FCanvas := Canvas;
+  FCanvas := Canvas
 end;
 
 procedure TMolChart.CalcScaleFactor;
@@ -49,19 +53,19 @@ begin
   xf := (x1-x0)/(R.Right-R.Left);
   lny0 := ln(y0);
   lny1 := ln(y1);
-  yf := (lny1-lny0)/(R.Top-R.Bottom);
+  yf := (lny1-lny0)/(R.Top-R.Bottom)
 end;
 
 function TMolChart.X2P(x: Double): Integer;
 {X scale value to chart pixel value}
 begin
-  Result := Trunc(R.Left+(x-x0)/xf);
+  Result := Trunc(R.Left+(x-x0)/xf)
 end;
 
 function TMolChart.Y2P(y: Double): Integer;
 {Y scale value to chart pixel value}
 begin
-  Result := Trunc(R.Bottom+(ln(y)-lny0)/yf);
+  Result := Trunc(R.Bottom+(ln(y)-lny0)/yf)
 end;
 
 function TMolChart.P2X(p: Integer): Double;
@@ -73,19 +77,19 @@ end;
 function TMolChart.P2Y(p: Integer): Double;
 {pixel point to y scale value}
 begin
-  Result := exp(lny0+(p-R.bottom)*yf);
+  Result := exp(lny0+(p-R.Bottom)*yf)
 end;
 
 procedure TMolChart.MoveTo(x, y: Double);
 {move to (x,y) scale position}
 begin
-  FCanvas.MoveTo(X2P(x), Y2P(y));
+  FCanvas.MoveTo(X2P(x), Y2P(y))
 end;
 
 procedure TMolChart.LineTo(x, y: Double);
 {line to (x,y) scale position}
 begin
-  FCanvas.LineTo(X2P(x), Y2P(y));
+  FCanvas.LineTo(X2P(x), Y2P(y))
 end;
 
 procedure TMolChart.DrawPoint(x, y: Double; Color: TColor);
@@ -96,7 +100,7 @@ begin
   FCanvas.Pen.Color := Color;
   px := X2P(x);
   py := Y2P(y);
-  FCanvas.Rectangle(px-1, py+1, px+1, py-1);
+  FCanvas.Rectangle(px-1, py+1, px+1, py-1)
 end;
 
 procedure TMolChart.DrawLine(x0, y0, x1, y1: Double; Color: TColor; PenWidth: Integer);
@@ -105,7 +109,7 @@ begin
   FCanvas.Pen.Color := Color;
   FCanvas.Pen.Width := PenWidth;
   MoveTo(x0, y0);
-  LineTo(x1, y1);
+  LineTo(x1, y1)
 end;
 
 procedure TMolChart.DrawVec(x, y: fvec; Color: TColor; PenWidth: Integer);
@@ -116,56 +120,62 @@ begin
   FCanvas.Pen.Color := Color;
   FCanvas.Pen.Width := PenWidth;
   MoveTo(x[0], y[0]);
-  for i := 1 to length(x)-1 do begin
-    LineTo(x[i], y[i]);
-    //DrawPoint(x[i], y[i], Color);
-  end;
+  for i := 1 to length(x)-1 do
+    LineTo(x[i], y[i])
 end;
 
-procedure TMolChart.DrawChart(ref: string; Width, Height: Integer);
+procedure TMolChart.DrawChart(ref: PAnsiChar; Width, Height: Integer);
 {draw the mollier chart}
 const
-  n=17;
+  n = 23;
 var
-  i: Integer;
+  i, Count: Integer;
   pc, tc, dc, hc: Double;
   p, hf, hg: fvec;
   dx, dy: Double;
+  _hf, _hg: Double;
 begin
   R := Rect(0, 0, Width, Height);
+  FCanvas.Brush.Color := $00f8fcfa;
+  FCanvas.FillRect(R);
   InflateRect(R, -FMargin, -FMargin);
   FCanvas.Pen.Color := clGray;
   FCanvas.Pen.Width := 1;
   FCanvas.Rectangle(R);
-  pc := 1e-6*Props1SI('PCRIT', CoolStr(ref));
-  tc := Props1SI('TCRIT', CoolStr(ref));
-  dc := Props1SI('RHOCRIT', CoolStr(ref));
-  try
-    // CoolProp crashes here! (with some fluids)
-    hc := 1e-3*PropsSI('H', 'T', tc, 'D', dc, CoolStr(ref));
-  except
-    Exit;
-  end;
-  y0 := 1;
+  pc := 1e-6*Props1SI('PCRIT', ref);
+  tc := Props1SI('TCRIT', ref);
+  dc := Props1SI('RHOCRIT', ref);
+  hc := 1e-3*PropsSI('H', 'T', tc, 'D', dc, ref);
+  y0 := 0.1;
   y1 := pc;
-  if y1 <= 1 then begin
-    // fluid critical pressure < 1 Mpa
-    Exit;
-  end;
-
+  // test for fluid critical pressure < 1 Mpa
+  if y1 <= y0 then y0 := 0.1*y1;
   // generate n points
   SetLength(p, n);
   SetLength(hf, n);
   SetLength(hg, n);
+  Count := 0;
   for i := 0 to n-1 do begin
     p[i] := y0 + i*(y1-y0)/n;
-    hf[i] := 1e-3*PropsSI('H', 'P', 1e6*p[i], 'Q', 0, CoolStr(ref));
-    hg[i] := 1e-3*PropsSI('H', 'P', 1e6*p[i], 'Q', 1, CoolStr(ref));
+    _hf := 1e-3*PropsSI('H', 'P', 1e6*p[i], 'Q', 0, ref);
+    _hg := 1e-3*PropsSI('H', 'P', 1e6*p[i], 'Q', 1, ref);
+    if IsInfinite(_hf) or IsInfinite(_hg) then continue;
+    if (_hf=0) or (_hg=0) then continue;
+    hf[Count] := _hf;
+    hg[Count] := _hg;
+    Count := Count + 1;
   end;
+  SetLength(p, Count);
+  SetLength(hf, Count);
+  SetLength(hg, Count);
 
   // adjust the scale
   x0 := MinValue(hf);
+  _hg := MinValue(hg);
+  if _hg < x0 then x0 := _hg;
   x1 := MaxValue(hg);
+  _hf := MaxValue(hf);
+  if _hf > x1 then x1 := _hf;
   dx := x1-x0;
   dy := y1-y0;
   x0 := x0-0.1*dx;
@@ -173,12 +183,9 @@ begin
   y1 := y1+0.1*dy;
   CalcScaleFactor;
 
-  // draw the liquid line
-  DrawVec(hf, p, clGreen, 2);
-
-  // draw the vapor line
-  DrawVec(hg, p, clRed, 2);
-  DrawPoint(hc, pc, clBlue);
+  DrawVec(hf, p, clGreen, 2); // liquid line
+  DrawVec(hg, p, clRed, 2); // vapor line
+  DrawPoint(hc, pc, clBlue) // critical point
 end;
 
 end.
