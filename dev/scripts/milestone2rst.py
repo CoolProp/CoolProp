625b00f8d8c290b0899e645e6f4dd17130a72f26
Ian Bell <ian.h.bell@gmail.com>
2016-05-10 22:12:51 -0600

    Prep for version 6 release

--- dev/scripts/milestone2rst.py
+++ dev/scripts/milestone2rst.py
@@ -1,7 +1,31 @@
 from __future__ import print_function
 import urllib, json, sys
 
-def generate(milestone):
+def generate_issues(milestone):
+    
+    # Find the milestone number for the given name
+    milestones_json = json.loads(urllib.urlopen('https://api.github.com/repos/CoolProp/CoolProp/milestones').read())
+
+    print(milestones_json)
+
+    # Map between name and number
+    title_to_number_map = {stone['title']: stone['number'] for stone in milestones_json}
+
+    # Find the desired number
+    number = title_to_number_map[milestone]
+
+    # Get the issues associated with the milestone
+    issues = json.loads(urllib.urlopen('https://api.github.com/repos/CoolProp/CoolProp/issues?state=all&per_page=1000&milestone='+str(number)).read())
+
+    # Make sure all issues are closed in this milestone
+    for issue in issues:
+        if issue['state'] != 'closed': raise ValueError('This issue is still open: ' + issue['title'])
+        
+    rst = 'Issues Closed:\n\n'+'\n'.join(['* `#{n:d} <https://github.com/CoolProp/CoolProp/issues/{n:d}>`_ : {t:s}'.format(n = issue['number'], t = issue['title']) for issue in issues])
+    
+    return rst
+
+def generate_PR(milestone):
     
     # Find the milestone number for the given name
     milestones_json = json.loads(urllib.urlopen('https://api.github.com/repos/CoolProp/CoolProp/milestones').read())
@@ -12,14 +36,13 @@ def generate(milestone):
     # Find the desired number
     number = title_to_number_map[milestone]
 
-    # Get the issues associated with the milestone
-    issues = json.loads(urllib.urlopen('https://api.github.com/repos/CoolProp/CoolProp/issues?state=all&per_page=100&milestone='+str(number)).read())
+    # Get the merged pull requests associated with the milestone
+    PR = json.loads(urllib.urlopen('https://api.github.com/repos/CoolProp/CoolProp/pulls?state=closed&per_page=1000&milestone='+str(number)).read())
 
-    # Make sure all issues are closed in this milestone
-    for issue in issues:
-        if issue['state'] != 'closed': raise ValueError('This issue is still open: ' + issue['title'])
+    for issue in PR:
+        print(issue)
         
-    rst = 'Issues Closed:\n\n'+'\n'.join(['* `#{n:d} <http://github.com/CoolProp/CoolProp/issues/{n:d}>`_ : {t:s}'.format(n = issue['number'], t = issue['title']) for issue in issues])
+    rst = 'Pull Requests merged:\n\n'+'\n'.join(['* `#{n:d} <https://github.com/CoolProp/CoolProp/pull/{n:d}>`_ : {t:s}'.format(n = issue['number'], t = issue['title'].encode('utf-8')) for issue in PR if issue['milestone']['title'] == milestone])
     
     return rst
 
@@ -27,4 +50,6 @@ if __name__=='__main__':
     if len(sys.argv) != 2:
         raise ValueError('This script should be called like this: python milestone2rst.py v5')
         
-    print(generate(sys.argv[1]))
+    print(generate_issues(sys.argv[1]))
+    print('')
+    print(generate_PR(sys.argv[1]))
