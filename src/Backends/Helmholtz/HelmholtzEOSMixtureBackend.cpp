1c79c77986663f0bc4322c22715ee472b06f344f
Ian Bell <ian.h.bell@gmail.com>
2016-10-24 18:29:19 -0600

    Actually check if T < Tmelt for p > pmin; #1288

--- src/Backends/Helmholtz/HelmholtzEOSMixtureBackend.cpp
+++ src/Backends/Helmholtz/HelmholtzEOSMixtureBackend.cpp
@@ -1436,21 +1436,27 @@ void HelmholtzEOSMixtureBackend::p_phase_determination_pure_or_pseudopure(int ot
         {
             case iT:
             {
-                if (_T < Tmin()){
-                    if (has_melting_line()){
-                        double Tm = melting_line(iT, iP, _p);
-                        if (get_config_bool(DONT_CHECK_PROPERTY_LIMITS)){
-                            _phase = iphase_liquid;
-                        }
-                        else{
-                            throw ValueError(format("For now, we don't support T [%g K] below Tmelt(p) [%g K]", _T, Tm));
-                        }
+
+                if (has_melting_line()){
+                    double Tm = melting_line(iT, iP, _p);
+                    if (get_config_bool(DONT_CHECK_PROPERTY_LIMITS)){
+                        _phase = iphase_liquid;
                     }
                     else{
-                        if (get_config_bool(DONT_CHECK_PROPERTY_LIMITS)){
-                            _phase = iphase_liquid;
+                        if (_T < Tm){
+                            throw ValueError(format("For now, we don't support T [%g K] below Tmelt(p) [%g K]", _T, Tm));
                         }
-                        else{
+                        if (_T < Tmin()){
+                            throw ValueError(format("For now, we don't support T [%g K] below Tmin(saturation) [%g K]", _T, Tmin()));
+                        }
+                    }
+                }
+                else{
+                    if (get_config_bool(DONT_CHECK_PROPERTY_LIMITS)){
+                        _phase = iphase_liquid;
+                    }
+                    else{
+                        if (_T < Tmin()){
                             throw ValueError(format("For now, we don't support T [%g K] below Tmin(saturation) [%g K]", _T, Tmin()));
                         }
                     }
