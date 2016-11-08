5f5ab4f8665c7f1a29e1c55e9ce4f4aee61e4442
Jeff Henning <jeff.p.henning@gmail.com>
2016-11-08 10:05:36 -0500

    Added PQ and QT Input Pairs to provide Saturation Values to IF97 Backend (#1323)
    * Added PQ and QT Input Pairs to provide Saturation Values
    
    * Removed semicolon from macro #undef NOMINMAX line 10 in CoolPropMathcad.cpp
    
    * Fixed some old tabification that crept back in.
    
    * Fixed calling order to enable output of Molar quantities
    
    * Rearranged Logic so Sat. vapor & Sat. liquid are iphase_two_phase.

--- src/Backends/IF97/IF97Backend.h
+++ src/Backends/IF97/IF97Backend.h
@@ -14,9 +14,9 @@ class IF97Backend : public AbstractState  {
 
 protected:
 
-	/// Additional cached elements used only in this backend since the "normal"
+    /// Additional cached elements used only in this backend since the "normal"
     /// backends use only molar units while IF97 uses mass-based units
-	CachedElement  _cpmass, _cvmass, _hmass, _rhomass, _smass, _umass;
+    CachedElement  _cpmass, _cvmass, _hmass, _rhomass, _smass, _umass;
 
 public:
     /// The name of the backend being used
@@ -31,59 +31,171 @@ public:
     void set_volu_fractions(const std::vector<CoolPropDbl> &volu_fractions){throw NotImplementedError("Volume composition has not been implemented.");};
     const std::vector<CoolPropDbl> & get_mole_fractions(void){throw NotImplementedError("get_mole_fractions composition has not been implemented.");};
 
-    /// Updating function for incompressible fluid
+    /// Override clear() function of IF97 Water
+    bool clear() {
+    // Reset all instances of CachedElement and overwrite
+    // the internal double values with -_HUGE
+    // Default phase condition is no phase imposed
+    // IF97 will make phase/region determination
+        this->_rhomolar = -_HUGE;
+        this->_T = -_HUGE;
+        this->_p = -_HUGE;
+        this->_Q = -_HUGE;
+        this->_phase = iphase_not_imposed; 
+        return true;
+    };
+
+    /// Updating function for IF97 Water
     /**
     In this function we take a pair of thermodynamic states, those defined in the input_pairs
-    enumeration and update all the internal variables that we can.
+    enumeration and update the bare minimum values needed to calculate the other values.
 
     @param input_pair Integer key from CoolProp::input_pairs to the two inputs that will be passed to the function
     @param value1 First input value
     @param value2 Second input value
     */
     void update(CoolProp::input_pairs input_pair, double value1, double value2){
+        
+        clear();  //clear the few cached values we are using
+
         switch(input_pair){
             case PT_INPUTS: _p = value1; _T = value2; break;
-            /* TODO:  Add input pairs for saturation functions 
-            case PQ_INPUTS: _p = value1; _Q = value2; break;
-            case QT_INPUTS: _Q = value1; _T = value2; break;
-            */
+            case PQ_INPUTS: 
+                _p = value1; 
+                _Q = value2; 
+                _T = IF97::Tsat97(_p);  // ...will throw exception if _P not on saturation curve
+                _phase = iphase_twophase;
+                break;
+            case QT_INPUTS: 
+                _Q = value1; 
+                _T = value2; 
+                _p = IF97::psat97(_T);  // ...will throw exception if _P not on saturation curve
+                _phase = iphase_twophase;
+                break;
             default:
-                throw ValueError("bad input_pair");
+                throw ValueError("Bad input_pair");
         }
     };
 
     /*  We have to override some of the functions from the AbstractState.
-	 *  IF97 is only mass-based and does not support conversion
-	 *  from mass- to molar-specific quantities.
-	 */
+     *  IF97 is only mass-based and does not support conversion
+     *  from mass- to molar-specific quantities.
+     */
     // ************************************************************************* //
     //                   Basic Thermodynamic Functions                           //
     // ************************************************************************* //
-	/// Return the mass density in kg/m^3
+    //
+    double calc_SatLiquid(parameters iCalc) {
+        switch(iCalc){
+        case iDmass:  return IF97::rholiq_p(_p); break; ///< Mass-based density
+        case iHmass:  return IF97::hliq_p(_p); break;   ///< Mass-based enthalpy
+        case iSmass:  return IF97::sliq_p(_p); break;   ///< Mass-based entropy
+        case iCpmass: return IF97::cpliq_p(_p); break;  ///< Mass-based constant-pressure specific heat
+        case iCvmass: return IF97::cvliq_p(_p); break;  ///< Mass-based constant-volume specific heat
+        case iUmass:  return IF97::uliq_p(_p); break;   ///< Mass-based internal energy
+        case ispeed_sound: return IF97::speed_soundliq_p(_p); break;  ///< Speed of Sound
+        default: return -_HUGE;
+        };
+    }
+    double calc_SatVapor(parameters iCalc) {
+        switch(iCalc){
+        case iDmass:  return IF97::rhovap_p(_p); break; ///< Mass-based density
+        case iHmass:  return IF97::hvap_p(_p); break;   ///< Mass-based enthalpy
+        case iSmass:  return IF97::svap_p(_p); break;   ///< Mass-based entropy
+        case iCpmass: return IF97::cpvap_p(_p); break;  ///< Mass-based constant-pressure specific heat
+        case iCvmass: return IF97::cvvap_p(_p); break;  ///< Mass-based constant-volume specific heat
+        case iUmass:  return IF97::uvap_p(_p); break;   ///< Mass-based internal energy
+        case ispeed_sound: return IF97::speed_soundvap_p(_p); break;  ///< Speed of Sound
+        default: return -_HUGE;
+        };
+    }
+    double calc_Flash(parameters iCalc) {
+        switch(_phase){
+            case iphase_twophase:   // In saturation envelope
+                if (std::abs(_Q) < 1e-10){
+                    return calc_SatLiquid(iCalc);    // bubble point (Q == 0) on Sat. Liquid curve
+                }
+                else if (std::abs(_Q-1) < 1e-10){
+                    return calc_SatVapor(iCalc);     // dew point (Q == 1) on Sat. Vapor curve
+                }
+                else {                               // else "inside" bubble ( 0 < Q < 1 )    
+                    switch(iCalc){ 
+                    case iDmass:
+                        return 1.0/(_Q/calc_SatVapor(iDmass) + (1.0-_Q)/calc_SatLiquid(iDmass)); break;
+                    case iCpmass:
+                        throw NotImplementedError(format("Isobaric Specific Heat not implemented in two phase region")); break;
+                    case iCvmass:
+                        throw NotImplementedError(format("Isochoric Specific Heat not implemented in two phase region")); break;
+                    case ispeed_sound:
+                        throw NotImplementedError(format("Speed of Sound not implemented in two phase region")); break;
+                    default:
+                        return _Q*calc_SatVapor(iCalc) + (1.0-_Q)*calc_SatLiquid(iCalc);
+                    };
+                }
+                break;
+            default:    // Outside saturation envelope (iphase_not_imposed), let IF97 determine phase/region
+                switch(iCalc){
+                case iDmass:  return IF97::rhomass_Tp(_T, _p); break; ///< Mass-based density
+                case iHmass:  return IF97::hmass_Tp(_T, _p); break;   ///< Mass-based enthalpy
+                case iSmass:  return IF97::smass_Tp(_T, _p); break;   ///< Mass-based entropy
+                case iCpmass: return IF97::cpmass_Tp(_T, _p); break;  ///< Mass-based constant-pressure specific heat
+                case iCvmass: return IF97::cvmass_Tp(_T, _p); break;  ///< Mass-based constant-volume specific heat
+                case iUmass:  return IF97::umass_Tp(_T, _p); break;   ///< Mass-based internal energy
+                case ispeed_sound: return IF97::speed_sound_Tp(_T, _p); break;  ///< speed of sound
+                default: throw NotImplementedError(format("Output variable not implemented in IF97 Backend"));
+                };
+        }
+    }
+    /// Return the mass density in kg/m³
     double rhomass(void){ return calc_rhomass(); };
-    double calc_rhomass(void){ return IF97::rhomass_Tp(_T, _p); };
-	/// Return the mass enthalpy in J/kg
-	double hmass(void){return calc_hmass();};
-    double calc_hmass(void){ return IF97::hmass_Tp(_T, _p); };
-	/// Return the molar entropy in J/mol/K
-	double smass(void){return calc_smass();};
-    double calc_smass(void){ return IF97::smass_Tp(_T, _p); };
-	/// Return the molar internal energy in J/mol
-	double umass(void){return calc_umass();};
-    double calc_umass(void){ return IF97::umass_Tp(_T, _p); };
-	/// Return the mass-based constant pressure specific heat in J/kg/K
-	double cpmass(void){return calc_cpmass();};
-    double calc_cpmass(void){ return IF97::cpmass_Tp(_T, _p); };
-    /// Return the mass-based constant volume specific heat in J/kg/K
-	double cvmass(void){return calc_cvmass();};
-    double calc_cvmass(void){ return IF97::cvmass_Tp(_T, _p); };
-    /// Return the speed of sound
-    double speed_sound(void){ return calc_speed_sound(); };
-    double calc_speed_sound(void) { return IF97::speed_sound_Tp(_T, _p); };
+    double calc_rhomass(void){ return calc_Flash(iDmass); };
+    /// Return the molar density in mol/m³
+    double rhomolar(void){ return calc_rhomolar(); };
+    double calc_rhomolar(void){ return rhomass()/molar_mass(); };    /// kg/m³ * mol/kg = mol/m³
 
+    /// Return the mass enthalpy in J/kg
+    double hmass(void){ return calc_hmass(); };
+    double calc_hmass(void){ return calc_Flash(iHmass); };
+    /// Return the molar enthalpy in J/mol
+    double hmolar(void){ return calc_hmolar(); };
+    double calc_hmolar(void){ return hmass()*molar_mass(); };        /// J/kg * kg/mol = J/mol
+
+    /// Return the mass entropy in J/kg/K
+    double smass(void){ return calc_smass(); };
+    double calc_smass(void){ return calc_Flash(iSmass); };
+    /// Return the molar entropy in J/mol/K
+    double smolar(void){ return calc_smolar(); };
+    double calc_smolar(void){ return smass()*molar_mass(); };        /// J/kg-K * kg/mol = J/mol-K
+
+    /// Return the mass internal energy in J/kg
+    double umass(void){ return calc_umass(); };
+    double calc_umass(void){ return calc_Flash(iUmass); };
+    /// Return the molar internal energy in J/mol
+    double umolar(void){ return calc_umolar(); };
+    double calc_umolar(void){ return umass()*molar_mass(); };        /// J/kg * kg/mol = J/mol
+
+    /// Return the mass-based constant pressure specific heat in J/kg/K
+    double cpmass(void){ return calc_cpmass(); };
+    double calc_cpmass(void){ return calc_Flash(iCpmass); };
+    /// Return the molar-based constant pressure specific heat in J/mol/K
+    double cpmolar(void){ return calc_cpmolar(); };
+    double calc_cpmolar(void){ return cpmass()*molar_mass(); };      /// J/kg-K * kg/mol = J/mol-K
+
+    /// Return the mass-based constant volume specific heat in J/kg/K
+    double cvmass(void){ return calc_cvmass(); };
+    double calc_cvmass(void){ return calc_Flash(iCvmass); };
+    /// Return the molar-based constant volume specific heat in J/mol/K
+    double cvmolar(void){ return calc_cvmolar(); };
+    double calc_cvmolar(void){ return cvmass()*molar_mass(); };      /// J/kg-K * kg/mol = J/mol-K
+
+    /// Return the speed of sound in m/s
+    double speed_sound(void){ return calc_speed_sound(); };
+    double calc_speed_sound(void) { return calc_Flash(ispeed_sound); };
+    //
     // ************************************************************************* //
     //                         Trivial Functions                                 //
     // ************************************************************************* //
+    //
     /// Using this backend, get the triple point temperature in K
     double calc_Ttriple(void){ return IF97::get_Ttrip(); };
     /// Using this backend, get the triple point pressure in Pa
@@ -119,7 +231,17 @@ public:
     // Overwrite the virtual calc_ functions for density
     double calc_rhomolar_critical(void){ return rhomass_critical()/molar_mass(); };
     double calc_rhomass_critical(void){ return IF97::get_rhocrit(); };
-
+    //
+    // ************************************************************************* //
+    //                      Saturation Functions                                 //
+    // ************************************************************************* //
+    //
+    double calc_pressure(void){ return _p; };
+    //
+    // ************************************************************************* //
+    //                      Saturation Functions                                 //
+    // ************************************************************************* //
+    //
 };
 
 } /* namespace CoolProp */
