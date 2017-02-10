7db398f56268e04eaf21b91931ad14e49780bcdd
JonWel <JonWel@users.noreply.github.com>
2017-02-10 08:11:42 +0100

    Removes some C4018 warnings,  #1413 (#1425)
    Should correct most of the remaining C4018

--- src/CoolPropLib.cpp
+++ src/CoolPropLib.cpp
@@ -642,16 +642,16 @@ EXPORT_CODE void CONVENTION AbstractState_get_phase_envelope_data(const long han
     try {
         shared_ptr<CoolProp::AbstractState> &AS = handle_manager.get(handle);
         CoolProp::PhaseEnvelopeData pe = AS->get_phase_envelope_data();
-        if (pe.T.size() > length){
+        if (pe.T.size() > static_cast<std::size_t>(length)){
             throw CoolProp::ValueError(format("Length of phase envelope vectors [%d] is greater than allocated buffer length [%d]", static_cast<int>(pe.T.size()), static_cast<int>(length)));
         }
         std::size_t N = pe.x.size();
-        for (int i = 0; i<pe.T.size(); i++){
+        for (std::size_t i = 0; i<pe.T.size(); i++){
             *(T+i) = pe.T[i];
             *(p+i) = pe.p[i];
             *(rhomolar_vap+i) = pe.rhomolar_vap[i];
             *(rhomolar_liq+i) = pe.rhomolar_liq[i];
-            for (int j = 0; j < N; ++j){
+            for (std::size_t j = 0; j < N; ++j){
                 *(x+i*N+j) = pe.x[j][i];
                 *(y+i*N+j) = pe.y[j][i];
             }
@@ -678,10 +678,10 @@ EXPORT_CODE void CONVENTION AbstractState_get_spinodal_data(const long handle, c
     try {
         shared_ptr<CoolProp::AbstractState> &AS = handle_manager.get(handle);
         CoolProp::SpinodalData spin = AS->get_spinodal_data();
-        if (spin.tau.size() > length){
+        if (spin.tau.size() > static_cast<std::size_t>(length)){
             throw CoolProp::ValueError(format("Length of spinodal vectors [%d] is greater than allocated buffer length [%d]", static_cast<int>(spin.tau.size()), static_cast<int>(length)));
         }
-        for (int i = 0; i<spin.tau.size(); ++i){
+        for (std::size_t i = 0; i<spin.tau.size(); ++i){
             *(tau+i) = spin.tau[i];
             *(delta+i) = spin.delta[i];
             *(M1+i) = spin.M1[i];
@@ -697,10 +697,10 @@ EXPORT_CODE void CONVENTION AbstractState_all_critical_points(const long handle,
     try {
         shared_ptr<CoolProp::AbstractState> &AS = handle_manager.get(handle);
         std::vector<CoolProp::CriticalState> pts = AS->all_critical_points();
-        if (pts.size() > length){
+        if (pts.size() > static_cast<std::size_t>(length)){
             throw CoolProp::ValueError(format("Length of critical point vector [%d] is greater than allocated buffer length [%d]", static_cast<int>(pts.size()), static_cast<int>(length)));
         }
-        for (int i = 0; i < pts.size(); ++i){
+        for (std::size_t i = 0; i < pts.size(); ++i){
             *(T+i) = pts[i].T;
             *(p+i) = pts[i].p;
             *(rhomolar+i) = pts[i].rhomolar;
