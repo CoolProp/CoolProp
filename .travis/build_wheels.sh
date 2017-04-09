85b3aad4f9c4b7acc6fcafc14d2591d8a4d3da27
Jorrit Wronski <jowr@users.noreply.github.com>
2017-04-09 23:27:26 +0200

    Create build_wheels.sh
    Started to integrate the wheel builders

---
+++ .travis/build_wheels.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+# https://github.com/pypa/python-manylinux-demo/blob/master/travis/build-wheels.sh
+set -e -x
+
+# Install a system package required by our library
+#yum install -y atlas-devel
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install cython wheel
+    "${PYBIN}/pip" wheel /io/wrappers/Python -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
+
+## Install packages and test
+#for PYBIN in /opt/python/*/bin/; do
+#    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
+#    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+#done
