44172d1df07fd7aef23c90936e839c2dc381a106
Jorrit Wronski <jowr@users.noreply.github.com>
2017-04-21 16:47:20 +0200

    Coverity scan (#1479)
    Added a basic Travis integration and added configuration details for coverity scans. Travis could be refined though.

--- Web/coolprop/snippets/propssi.cxx
+++ Web/coolprop/snippets/propssi.cxx
@@ -1,5 +1,6 @@
 #include "CoolProp.h"
 #include <iostream>
+#include <stdlib.h>
 using namespace CoolProp;
 int main()
 {
@@ -7,7 +8,7 @@ int main()
     std::cout << PropsSI("Dmolar","T",298,"P",1e5,"Propane[0.5]&Ethane[0.5]") << std::endl; // Default backend is HEOS
     std::cout << PropsSI("Dmolar","T",298,"P",1e5,"HEOS::Propane[0.5]&Ethane[0.5]") << std::endl;
     std::cout << PropsSI("Dmolar","T",298,"P",1e5,"REFPROP::Propane[0.5]&Ethane[0.5]") << std::endl;
-
+    // Vector example
     std::vector<double> z(2,0.5);
     // Second type (C++ only, a bit faster, allows for vector inputs and outputs)
     std::vector<std::string> fluids; fluids.push_back("Propane"); fluids.push_back("Ethane");
@@ -17,6 +18,6 @@ int main()
     std::cout << PropsSImulti(outputs,"T", T, "P", p, "HEOS", fluids, z)[0][0] << std::endl;
     // Comment me out if REFPROP is not installed
     std::cout << PropsSImulti(outputs,"T", T, "P", p, "REFPROP", fluids, z)[0][0] << std::endl;
-    
+    // All done return
     return EXIT_SUCCESS;
 }
