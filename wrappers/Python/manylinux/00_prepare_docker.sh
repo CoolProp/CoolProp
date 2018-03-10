aaea205704f799c20d0f2debc72f9b4b1af7b1a5
Ian Bell <ian.h.bell@gmail.com>
2018-03-10 14:44:36 -0700

    No more need to kill the image?

--- wrappers/Python/manylinux/00_prepare_docker.sh
+++ wrappers/Python/manylinux/00_prepare_docker.sh
@@ -55,10 +55,6 @@ pushd ${CUR_DIR}/../../..
 
 # Run the build script
 chmod +x ${CUR_DIR}/01_build_wheels.sh
-docker ps
-docker rm -f manylinux
-docker kill manylinux
-docker stop manylinux 
 # docker run --rm -v `pwd`:/io ${DOCKER_IMG_NAME}:${DOCKER_MACHINE_TAG} /io/wrappers/Python/manylinux/01_build_wheels.sh ${SETUP_PY_ARGS}
 docker run -itd --name manylinux ${DOCKER_IMG_NAME}:${DOCKER_MACHINE_TAG} bash
 docker cp . manylinux:/io
