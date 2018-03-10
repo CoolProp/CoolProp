adb8519ae4793aaab99a67ebb2b9511dd1231959
Ian Bell <ian.h.bell@gmail.com>
2018-03-10 16:01:01 -0700

    Why is python 3.6 build not happening?

--- wrappers/Python/manylinux/00_prepare_docker.sh
+++ wrappers/Python/manylinux/00_prepare_docker.sh
@@ -56,6 +56,7 @@ pushd ${CUR_DIR}/../../..
 # Run the build script
 chmod +x ${CUR_DIR}/01_build_wheels.sh
 # docker run --rm -v `pwd`:/io ${DOCKER_IMG_NAME}:${DOCKER_MACHINE_TAG} /io/wrappers/Python/manylinux/01_build_wheels.sh ${SETUP_PY_ARGS}
+docker run -itd --name manylinux ${DOCKER_IMG_NAME}:${DOCKER_MACHINE_TAG} bash ls /py*
 docker run -itd --name manylinux ${DOCKER_IMG_NAME}:${DOCKER_MACHINE_TAG} bash
 docker cp . manylinux:/io
 docker exec manylinux /io/wrappers/Python/manylinux/01_build_wheels.sh ${SETUP_PY_ARGS}
