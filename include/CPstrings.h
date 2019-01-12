b1fd926ef3d1c0aa57a3e23ebeddc7d6da05a4c2
Ian Bell <ian.h.bell@gmail.com>
2019-01-11 20:01:01 -0700

    First attempt at moving FMTLIB away from header-only; speed up builds maybe?

--- include/CPstrings.h
+++ include/CPstrings.h
@@ -7,12 +7,12 @@
     #include <functional>
 
 #if !defined(NO_FMTLIB)
-    #ifndef FMT_HEADER_ONLY
-    #define FMT_HEADER_ONLY
-    #endif
+    //#ifndef FMT_HEADER_ONLY
+    //#define FMT_HEADER_ONLY
+    //#endif
     #include "fmt/format.h" // For addition of the string formatting functions and macros from fmtlib
     #include "fmt/printf.h" // For sprintf
-    #undef FMT_HEADER_ONLY
+    //#undef FMT_HEADER_ONLY
 #else
     #include <vector>
     #include <string>
