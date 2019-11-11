22c2800d5280e8afb2ac592f4036d2816b346514
ian <ian.h.bell@gmail.com>
2019-11-10 20:38:23 -0700

    Some info

--- src/Tests/catch_always_return_success.cxx
+++ src/Tests/catch_always_return_success.cxx
@@ -1,3 +1,7 @@
+// This file is used in tests to always return success
+// so that address saniter's return code will be the 
+// return code that is fed back to buildbot, not the
+// number of failing catch tests
 #include "catch.hpp"
 
 int main (int argc, char * argv[]) {
