be231af1613918b5786f2a150910023f047bf640
ian <ian.h.bell@gmail.com>
2019-11-10 20:30:22 -0700

    ASAN leaks will actually get tested

--- dev/asan/run_asan.sh
+++ dev/asan/run_asan.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-ls /
 mkdir /cpbuild && cd /cpbuild
 cmake /coolprop -DCOOLPROP_CLANG_ADDRESS_SANITIZER=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_C_COMPILER=/usr/bin/clang-9 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-9
-#DYLD_LIBRARY_PATH=/usr/lib/clang/3.9.0/lib/darwin/ ASAN_SYMBOLIZER_PATH=/usr/bin/llvm-symbolizer  ASAN_OPTIONS=verbosity=1 cmake --build .
-LSAN_OPTIONS=verbosity=1:log_threads=1 cmake --build .
+ASAN_OPTIONS=verbosity=1 cmake --build .
