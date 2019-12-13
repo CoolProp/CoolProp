d84774d0998aca9c5b728cabea00f98ba19f9c0a
ian <ian.h.bell@gmail.com>
2019-12-12 23:01:53 -0700

    another try with shared_ptr

--- include/crossplatform_shared_ptr.h
+++ include/crossplatform_shared_ptr.h
@@ -8,24 +8,27 @@
 #if defined(__ANDROID__)
         #include <memory>
         using std::shared_ptr;
-#elif defined(__ISLINUX__)
+#elif defined(__ISLINUX__) && (defined(__llvm__) || defined(__clang__)) // CLANG
     #if __has_include(<tr1/memory>)
         // CLANG and -stdlib=libstdc++
         // See also http://stackoverflow.com/questions/13445742/apple-and-shared-ptr
         #include <tr1/memory>
-        using namespace std::tr1;
+        using std::tr1::shared_ptr;
     #else
         // CLANG and -stdlib=libc++
         #include <memory>
         using std::shared_ptr;
     #endif
+#elif defined(__ISLINUX__) // GCC
+    #include <memory>
+    using std::shared_ptr;
 #elif defined(__ISAPPLE__) && (defined(__llvm__) || defined(__clang__)) // CLANG
     // See docs for clang: http://clang.llvm.org/docs/LanguageExtensions.html#include-file-checking-macros
     #if __has_include(<tr1/memory>)
         // CLANG and -stdlib=libstdc++
         // See also http://stackoverflow.com/questions/13445742/apple-and-shared-ptr
         #include <tr1/memory>
-        using namespace std::tr1;
+        using std::tr1::shared_ptr;
     #else
         // CLANG and -stdlib=libc++
         #include <memory>
@@ -33,17 +36,17 @@
     #endif
 #elif defined(__GNUC__)
     #include <tr1/memory>
-    using namespace std::tr1;
+    using std::tr1::shared_ptr;
 #elif defined(__ISWINDOWS__) && defined(__MINGW32__)
     #include <tr1/memory>
-    using namespace std::tr1;
+    using std::tr1;
 #elif defined(__ISWINDOWS__) && !defined(__MINGW32__)
     #include <memory>
     // VS2008 has std::shared_ptr from C++11
     #if defined(_MSC_VER) && _MSC_VER >= 1600
         using std::shared_ptr;
     #else
-        using namespace std::tr1;
+        using std::tr1::shared_ptr;
     #endif
 #else
     #pragma error
