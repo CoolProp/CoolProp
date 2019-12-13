86c01b884207430ec40ca24ce3d39a496451615d
ian <ian.h.bell@gmail.com>
2019-12-12 23:22:22 -0700

    Seems like maybe we can remove all the references to std::tr1::shared_ptr

--- include/crossplatform_shared_ptr.h
+++ include/crossplatform_shared_ptr.h
@@ -3,25 +3,29 @@
 
 #include "PlatformDetermination.h"
 
+#include <memory>
+using std::shared_ptr;
+        
+/*
 // Based on the platform and compiler, include the necessary header to give access to std::tr1::shared_ptr directly as shared_ptr
 
 #if defined(__ANDROID__)
         #include <memory>
         using std::shared_ptr;
 #elif defined(__ISLINUX__) && (defined(__llvm__) || defined(__clang__)) // CLANG
-    #if __has_include(<tr1/memory>)
-        // CLANG and -stdlib=libstdc++
-        // See also http://stackoverflow.com/questions/13445742/apple-and-shared-ptr
-        #include <tr1/memory>
-        using std::tr1::shared_ptr;
-    #else
+    // #if __has_include(<tr1/memory>)
+    //     // CLANG and -stdlib=libstdc++
+    //     // See also http://stackoverflow.com/questions/13445742/apple-and-shared-ptr
+    //     #include <tr1/memory>
+    //     using std::tr1::shared_ptr;
+    // #else
         // CLANG and -stdlib=libc++
         #include <memory>
         using std::shared_ptr;
-    #endif
+    // #endif
 #elif defined(__ISLINUX__) // GCC
     #include <memory>
-    using std::shared_ptr;
+    using shared_ptr = std::shared_ptr;
 #elif defined(__ISAPPLE__) && (defined(__llvm__) || defined(__clang__)) // CLANG
     // See docs for clang: http://clang.llvm.org/docs/LanguageExtensions.html#include-file-checking-macros
     #if __has_include(<tr1/memory>)
@@ -52,6 +56,6 @@
     #pragma error
 #endif
 
-
+*/
 
 #endif
