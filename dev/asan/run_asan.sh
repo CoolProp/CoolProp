564a1b1a6a487c30a146388488688c62b997a97a
Ian Bell <ian.h.bell@gmail.com>
2020-04-30 20:56:59 -0400

    Get address sanitizer running again

--- dev/asan/run_asan.sh
+++ dev/asan/run_asan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir /cpbuild || 0
 cd /cpbuild
-cmake /coolprop -DCOOLPROP_CLANG_ADDRESS_SANITIZER=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_C_COMPILER=/usr/bin/clang-9 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-9
+ls /usr/bin/clang*
+cmake /coolprop -DCOOLPROP_CLANG_ADDRESS_SANITIZER=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_C_COMPILER=/usr/bin/clang-10 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-10 -DPYTHON_EXECUTABLE=/usr/bin/python3
 ASAN_OPTIONS=verbosity=1 cmake --build .
