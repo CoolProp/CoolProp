cd104e7a0d2821866c42cc4629c9354982835f68
Joel Martin <39242266+joel-martin@users.noreply.github.com>
2021-12-13 16:42:13 +0100

    Update example for bicubic tabular backend.

--- dev/TabularBackend/bicubic.cxx
+++ dev/TabularBackend/bicubic.cxx
@@ -7,6 +7,8 @@
 using namespace CoolProp;
 int main()
 {
+	set_debug_level(0);
+
     // Low level interface
     shared_ptr<AbstractState> Bicubic(AbstractState::factory("BICUBIC&HEOS","n-Heptane"));
     Bicubic->update(DmassT_INPUTS, 4.2692360381933803, 372.49600373915791); // SI units
@@ -29,6 +31,26 @@ int main()
 
     TabularDataSet data_set;
     data_set.build_tables(AS);
+
+    std::cout << "Bicubic" << std::endl;
+
+    std::cout << Bicubic->rhomolar() << std::endl;
+
+    Bicubic->update(DmolarUmolar_INPUTS, 2.7635029493162853e-07, 13886.488232581964);
+
+    std::cout << "rhomolar = " << Bicubic->rhomolar() << ", umolar = " << Bicubic->umolar() << std::endl;
+
+
+    std::cout << "p = " << Bicubic->p() << ", T = " << Bicubic->T() << std::endl;
+    std::cout << "hmolar = " << Bicubic->hmolar() << ", smolar = " << Bicubic->smolar() << std::endl;
+
+    /*
+
+    Bicubic->update(DmassUmass_INPUTS, Bicubic->rhomass(), 2.0*Bicubic->umass());
+
+    std::cout << Bicubic->p() << "\t" << Bicubic->T() << "\t" << Bicubic->hmass() << std::endl;
+
+	*/
 
     std::cout << "Done" << std::endl;
 
