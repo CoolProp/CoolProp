c7e8bf66694b6791ffbff5cf42ea7080978b26e8
Jorrit Wronski <jowr@ipu.dk>
2022-12-01 14:45:59 +0100

    Simplify the doc build process

--- dev/scripts/examples/LinuxRun.py
+++ dev/scripts/examples/LinuxRun.py
@@ -38,48 +38,48 @@ if __name__ == '__main__':
         tee_call('python Example.py', fp, shell=True, cwd='Python')
     copyfiles('Python', 'py')
 
-    if not os.path.exists('Octave'): os.mkdir('Octave')
-    O = Octave()
-    O.write('Octave/Example.m', O.parse())
-    kwargs = dict(stdout=sys.stdout, stderr=sys.stderr, shell=True, cwd='Octave')
-    subprocess.check_call('cmake ../../../.. -DCOOLPROP_OCTAVE_MODULE=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCOOLPROP_NO_EXAMPLES=ON', **kwargs)
-    subprocess.check_call('cmake --build .', **kwargs)
-    with codecs.open('Octave/Example.out', 'w', encoding='utf-8') as fp:
-        tee_call(r'octave Example.m', fp, shell=True, cwd='Octave')
-    copyfiles('Octave', 'm')
-
-    if not os.path.exists('Java'): os.mkdir('Java')
-    J = Java()
-    J.write('Java/Example.java', J.parse())
-    kwargs = dict(stdout=sys.stdout, stderr=sys.stderr, shell=True, cwd='Java')
-    subprocess.check_call('cmake ../../../.. -DCOOLPROP_JAVA_MODULE=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCOOLPROP_NO_EXAMPLES=ON', **kwargs)
-    subprocess.check_call('cmake --build .', **kwargs)
-    subprocess.check_call(r'javac *.java', **kwargs)
-    with codecs.open('Java/Example.out', 'w', encoding='utf-8') as fp:
-        tee_call(r'java -Djava.library.path="'+os.path.abspath('Java')+'" Example', fp, shell=True, cwd='Java')
-    copyfiles('Java', 'java')
-
-    if not os.path.exists('Csharp'): os.mkdir('Csharp')
-    C = Csharp()
-    C.write('Csharp/Example.cs', C.parse())
-    kwargs = dict(stdout=sys.stdout, stderr=sys.stderr, shell=True, cwd='Csharp')
-    subprocess.check_call('cmake ../../../.. -DCOOLPROP_CSHARP_MODULE=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCOOLPROP_NO_EXAMPLES=ON', **kwargs)
-    subprocess.check_call('cmake --build .', **kwargs)
-    subprocess.check_call(r'mcs -out:Example *.cs', **kwargs)
-    with codecs.open('Csharp/Example.out', 'w', encoding='utf-8') as fp:
-        tee_call(r'mono Example', fp, shell=True, cwd='Csharp')
-    copyfiles('Csharp', 'cs')
-
-    if not os.path.exists('R'): os.mkdir('R')
-    RR = R()
-    RR.write('R/Example.R', RR.parse())
-    kwargs = dict(stdout=sys.stdout, stderr=sys.stderr, shell=True, cwd='R')
-    subprocess.check_call('cmake ../../../.. -DCOOLPROP_R_MODULE=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCOOLPROP_NO_EXAMPLES=ON -DR_BIN=/usr/bin', **kwargs)
-    subprocess.check_call('cmake --build .', **kwargs)
-    with codecs.open('R/Example.out', 'w', encoding='utf-8') as fp:
-        tee_call(r'DYLD_LIBRARY_PATH=/opt/refprop Rscript Example.R', fp, shell=True, cwd='R')
-    copyfiles('R', 'R')
-
+    #if not os.path.exists('Octave'): os.mkdir('Octave')
+    #O = Octave()
+    #O.write('Octave/Example.m', O.parse())
+    #kwargs = dict(stdout=sys.stdout, stderr=sys.stderr, shell=True, cwd='Octave')
+    #subprocess.check_call('cmake ../../../.. -DCOOLPROP_OCTAVE_MODULE=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCOOLPROP_NO_EXAMPLES=ON', **kwargs)
+    #subprocess.check_call('cmake --build .', **kwargs)
+    #with codecs.open('Octave/Example.out', 'w', encoding='utf-8') as fp:
+    #    tee_call(r'octave Example.m', fp, shell=True, cwd='Octave')
+    #copyfiles('Octave', 'm')
+    #
+    #if not os.path.exists('Java'): os.mkdir('Java')
+    #J = Java()
+    #J.write('Java/Example.java', J.parse())
+    #kwargs = dict(stdout=sys.stdout, stderr=sys.stderr, shell=True, cwd='Java')
+    #subprocess.check_call('cmake ../../../.. -DCOOLPROP_JAVA_MODULE=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCOOLPROP_NO_EXAMPLES=ON', **kwargs)
+    #subprocess.check_call('cmake --build .', **kwargs)
+    #subprocess.check_call(r'javac *.java', **kwargs)
+    #with codecs.open('Java/Example.out', 'w', encoding='utf-8') as fp:
+    #    tee_call(r'java -Djava.library.path="'+os.path.abspath('Java')+'" Example', fp, shell=True, cwd='Java')
+    #copyfiles('Java', 'java')
+    #
+    #if not os.path.exists('Csharp'): os.mkdir('Csharp')
+    #C = Csharp()
+    #C.write('Csharp/Example.cs', C.parse())
+    #kwargs = dict(stdout=sys.stdout, stderr=sys.stderr, shell=True, cwd='Csharp')
+    #subprocess.check_call('cmake ../../../.. -DCOOLPROP_CSHARP_MODULE=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCOOLPROP_NO_EXAMPLES=ON', **kwargs)
+    #subprocess.check_call('cmake --build .', **kwargs)
+    #subprocess.check_call(r'mcs -out:Example *.cs', **kwargs)
+    #with codecs.open('Csharp/Example.out', 'w', encoding='utf-8') as fp:
+    #    tee_call(r'mono Example', fp, shell=True, cwd='Csharp')
+    #copyfiles('Csharp', 'cs')
+    #
+    #if not os.path.exists('R'): os.mkdir('R')
+    #RR = R()
+    #RR.write('R/Example.R', RR.parse())
+    #kwargs = dict(stdout=sys.stdout, stderr=sys.stderr, shell=True, cwd='R')
+    #subprocess.check_call('cmake ../../../.. -DCOOLPROP_R_MODULE=ON -DCMAKE_VERBOSE_MAKEFILE=ON -DCOOLPROP_NO_EXAMPLES=ON -DR_BIN=/usr/bin', **kwargs)
+    #subprocess.check_call('cmake --build .', **kwargs)
+    #with codecs.open('R/Example.out', 'w', encoding='utf-8') as fp:
+    #    tee_call(r'DYLD_LIBRARY_PATH=/opt/refprop Rscript Example.R', fp, shell=True, cwd='R')
+    #copyfiles('R', 'R')
+    #
     #if not os.path.exists('MATLAB'): os.mkdir('MATLAB')
     #M = MATLAB()
     #M.write('MATLAB/Example.m', M.parse())
