a6f9bf635a218aa0e3048fb1c9bb5317cd066592
Jorrit Wronski <jowr@ipu.dk>
2023-11-24 13:45:58 +0100

    Directly include the food properties using the coefficients from Choi and Okos (1986)

--- dev/incompressible_liquids/CPIncomp/CoefficientFluids.py
+++ dev/incompressible_liquids/CPIncomp/CoefficientFluids.py
@@ -39,3 +39,159 @@ class NitrateSalt(PureData, CoefficientData):
         self.viscosity.type = self.viscosity.INCOMPRESSIBLE_POLYNOMIAL
         self.viscosity.source = self.viscosity.SOURCE_COEFFS
         self.viscosity.coeffs = np.array([[22.714], [-0.120], [2.281 * 1e-4], [-1.474 * 1e-7]]) / 1e3
+
+
+class FoodProtein(PureData, CoefficientData):
+
+    def __init__(self):
+        CoefficientData.__init__(self)
+        PureData.__init__(self)
+        self.name = "FoodProtein"
+        self.description = "Food protein model from the 2006 ASHRAE Handbook based on data from Choi and Okos (1986)"
+        self.reference = "ASHRAE2006"
+
+        self.Tmin = -40 + 273.15
+        self.Tmax = 150 + 273.15
+        self.TminPsat = self.Tmax
+
+        self.conductivity.source = self.conductivity.SOURCE_COEFFS
+        self.conductivity.type = self.conductivity.INCOMPRESSIBLE_POLYNOMIAL
+        self.conductivity.coeffs = np.array([[1.7881e-1], [1.1958e-3], [-2.7178e-6]])
+
+        self.density.source = self.density.SOURCE_COEFFS
+        self.density.type = self.density.INCOMPRESSIBLE_POLYNOMIAL
+        self.density.coeffs = np.array([[1.3299e3], [-5.1840]])
+
+        self.specific_heat.source = self.specific_heat.SOURCE_COEFFS
+        self.specific_heat.type = self.specific_heat.INCOMPRESSIBLE_POLYNOMIAL
+        self.specific_heat.coeffs = np.array([[2.0082], [1.2089e-3], [-1.3129-6]])
+        
+
+class FoodFat(PureData, CoefficientData):
+
+    def __init__(self):
+        CoefficientData.__init__(self)
+        PureData.__init__(self)
+        self.name = "FoodFat"
+        self.description = "Food fat model from the 2006 ASHRAE Handbook based on data from Choi and Okos (1986)"
+        self.reference = "ASHRAE2006"
+
+        self.Tmin = -40 + 273.15
+        self.Tmax = 150 + 273.15
+        self.TminPsat = self.Tmax
+
+        self.conductivity.source = self.conductivity.SOURCE_COEFFS
+        self.conductivity.type = self.conductivity.INCOMPRESSIBLE_POLYNOMIAL
+        self.conductivity.coeffs = np.array([[1.8071e-1], [-2.7604e-4], [-1.7749e-7]])
+                                                                         
+        self.density.source = self.density.SOURCE_COEFFS
+        self.density.type = self.density.INCOMPRESSIBLE_POLYNOMIAL
+        self.density.coeffs = np.array([[9.2559e2], [-4.1757]])
+
+        self.specific_heat.source = self.specific_heat.SOURCE_COEFFS
+        self.specific_heat.type = self.specific_heat.INCOMPRESSIBLE_POLYNOMIAL
+        self.specific_heat.coeffs = np.array([[1.9842], [1.4733e-3], [-4.8008e-6]])
+
+
+class FoodCarbohydrate(PureData, CoefficientData):
+
+    def __init__(self):
+        CoefficientData.__init__(self)
+        PureData.__init__(self)
+        self.name = "FoodCarbohydrate"
+        self.description = "Food carbohydrate model from the 2006 ASHRAE Handbook based on data from Choi and Okos (1986)"
+        self.reference = "ASHRAE2006"
+
+        self.Tmin = -40 + 273.15
+        self.Tmax = 150 + 273.15
+        self.TminPsat = self.Tmax
+
+        self.conductivity.source = self.conductivity.SOURCE_COEFFS
+        self.conductivity.type = self.conductivity.INCOMPRESSIBLE_POLYNOMIAL
+        self.conductivity.coeffs = np.array([[2.0141e-1], [1.3874e-3], [-4.3312e-6]])
+
+        self.density.source = self.density.SOURCE_COEFFS
+        self.density.type = self.density.INCOMPRESSIBLE_POLYNOMIAL
+        self.density.coeffs = np.array([[1.5991e3], [-3.1046]])
+
+        self.specific_heat.source = self.specific_heat.SOURCE_COEFFS
+        self.specific_heat.type = self.specific_heat.INCOMPRESSIBLE_POLYNOMIAL
+        self.specific_heat.coeffs = np.array([[1.5488], [1.9625e-3], [-5.9399e-6]])
+
+
+class FoodFiber(PureData, CoefficientData):
+
+    def __init__(self):
+        CoefficientData.__init__(self)
+        PureData.__init__(self)
+        self.name = "FoodFiber"
+        self.description = "Food fiber model from the 2006 ASHRAE Handbook based on data from Choi and Okos (1986)"
+        self.reference = "ASHRAE2006"
+
+        self.Tmin = -40 + 273.15
+        self.Tmax = 150 + 273.15
+        self.TminPsat = self.Tmax
+
+        self.conductivity.source = self.conductivity.SOURCE_COEFFS
+        self.conductivity.type = self.conductivity.INCOMPRESSIBLE_POLYNOMIAL
+        self.conductivity.coeffs = np.array([[1.8331e-1], [1.2497e-3], [-3.1683e-6]])
+
+        self.density.source = self.density.SOURCE_COEFFS
+        self.density.type = self.density.INCOMPRESSIBLE_POLYNOMIAL
+        self.density.coeffs = np.array([[1.3115e3], [-3.6589]])
+
+        self.specific_heat.source = self.specific_heat.SOURCE_COEFFS
+        self.specific_heat.type = self.specific_heat.INCOMPRESSIBLE_POLYNOMIAL
+        self.specific_heat.coeffs = np.array([[1.8459], [1.8306e-3], [-4.6509e-6]])
+                                                                      
+
+class FoodAsh(PureData, CoefficientData):
+
+    def __init__(self):
+        CoefficientData.__init__(self)
+        PureData.__init__(self)
+        self.name = "FoodAsh"
+        self.description = "Food ash model from the 2006 ASHRAE Handbook based on data from Choi and Okos (1986)"
+        self.reference = "ASHRAE2006"
+
+        self.Tmin = -40 + 273.15
+        self.Tmax = 150 + 273.15
+        self.TminPsat = self.Tmax
+
+        self.conductivity.source = self.conductivity.SOURCE_COEFFS
+        self.conductivity.type = self.conductivity.INCOMPRESSIBLE_POLYNOMIAL
+        self.conductivity.coeffs = np.array([[3.2962e-1], [1.4011e-3], [-2.9069e-6]])
+
+        self.density.source = self.density.SOURCE_COEFFS
+        self.density.type = self.density.INCOMPRESSIBLE_POLYNOMIAL
+        self.density.coeffs = np.array([[2.4238e3], [-2.8063]])
+
+        self.specific_heat.source = self.specific_heat.SOURCE_COEFFS
+        self.specific_heat.type = self.specific_heat.INCOMPRESSIBLE_POLYNOMIAL
+        self.specific_heat.coeffs = np.array([[1.0926], [1.8896e-3], [-3.6817e-6]])
+
+
+class FoodWater(PureData, CoefficientData):
+    
+    def __init__(self):
+        CoefficientData.__init__(self)
+        PureData.__init__(self)
+        self.name = "FoodWater"
+        self.description = "Food water model from the 2006 ASHRAE Handbook based on data from Choi and Okos (1986)"
+        self.reference = "ASHRAE2006"
+
+        self.Tmin = -40 + 273.15
+        self.Tmax = 150 + 273.15
+        self.TminPsat = self.Tmax
+
+        self.conductivity.source = self.conductivity.SOURCE_COEFFS
+        self.conductivity.type = self.conductivity.INCOMPRESSIBLE_POLYNOMIAL
+        self.conductivity.coeffs = np.array([[5.7109e-1], [1.7625e-3], [-6.7036e-6]])
+
+        self.density.source = self.density.SOURCE_COEFFS
+        self.density.type = self.density.INCOMPRESSIBLE_POLYNOMIAL
+        self.density.coeffs = np.array([[9.9718e2], [3.1439e-3], [-3.7574e-3]])
+
+        self.specific_heat.source = self.specific_heat.SOURCE_COEFFS
+        self.specific_heat.type = self.specific_heat.INCOMPRESSIBLE_POLYNOMIAL
+        self.specific_heat.coeffs = np.array([[4.1289], [-9.0864e-5], [5.4731e-6]])
