53b62ab3aa48ee0d481fb838fe174d275876c79a
Jorrit Wronski <jowr@ipu.dk>
2023-11-28 16:44:36 +0100

    use interpolation to avoid the division by zero when calculating incompressible fluid properties exactly at the base temperature or composition #2295

--- src/PolyMath.cpp
+++ src/PolyMath.cpp
@@ -17,6 +17,8 @@
 
 namespace CoolProp {
 
+const double CPPOLY_EPSILON = DBL_EPSILON * 100.0;
+
 /// Basic checks for coefficient vectors.
 /** Starts with only the first coefficient dimension
  *  and checks the matrix size against the parameters rows and columns.
@@ -462,9 +464,14 @@ double Polynomial2DFrac::evaluate(const Eigen::MatrixXd& coefficients, const dou
         throw ValueError(format("%s (%d): You have a 2D coefficient matrix (%d,%d), please use the 2D functions. ", __FILE__, __LINE__,
                                 coefficients.rows(), coefficients.cols()));
     }
-    if ((firstExponent < 0) && (std::abs(x_in - x_base) < DBL_EPSILON)) {
-        throw ValueError(
-          format("%s (%d): A fraction cannot be evaluated with zero as denominator, x_in-x_base=%f ", __FILE__, __LINE__, x_in - x_base));
+    if ((firstExponent < 0) && (std::abs(x_in - x_base) < CPPOLY_EPSILON)) {
+        //throw ValueError(format("%s (%d): A fraction cannot be evaluated with zero as denominator, x_in-x_base=%f ", __FILE__, __LINE__, x_in - x_base));
+        const double x_lo = x_base - 1.001*CPPOLY_EPSILON;
+        const double x_hi = x_base + 1.001*CPPOLY_EPSILON;
+        const double y_lo = evaluate(coefficients, x_lo, firstExponent, x_base);
+        const double y_hi = evaluate(coefficients, x_hi, firstExponent, x_base);
+        const double m = (y_hi - y_lo)/(x_hi - x_lo);
+        return y_lo + m*(x_in - x_lo);
     }
 
     Eigen::MatrixXd tmpCoeffs(coefficients);
@@ -506,13 +513,23 @@ double Polynomial2DFrac::evaluate(const Eigen::MatrixXd& coefficients, const dou
 /// @param y_base double value that represents the base value for a centred fit in the 2nd dimension
 double Polynomial2DFrac::evaluate(const Eigen::MatrixXd& coefficients, const double& x_in, const double& y_in, const int& x_exp, const int& y_exp,
                                   const double& x_base, const double& y_base) {
-    if ((x_exp < 0) && (std::abs(x_in - x_base) < DBL_EPSILON)) {
-        throw ValueError(
-          format("%s (%d): A fraction cannot be evaluated with zero as denominator, x_in-x_base=%f ", __FILE__, __LINE__, x_in - x_base));
+    if ((x_exp < 0) && (std::abs(x_in - x_base) < CPPOLY_EPSILON)) {
+        // throw ValueError(format("%s (%d): A fraction cannot be evaluated with zero as denominator, x_in-x_base=%f ", __FILE__, __LINE__, x_in - x_base));
+        double x_lo = x_base - 1.001*CPPOLY_EPSILON;
+        double x_hi = x_base + 1.001*CPPOLY_EPSILON;
+        double z_lo = evaluate(coefficients, x_lo, y_in, x_exp, y_exp, x_base, y_base);
+        double z_hi = evaluate(coefficients, x_hi, y_in, x_exp, y_exp, x_base, y_base);
+        double dzdx = (z_hi - z_lo)/(x_hi - x_lo);
+        return z_lo + dzdx*(x_in - x_lo);
     }
-    if ((y_exp < 0) && (std::abs(y_in - y_base) < DBL_EPSILON)) {
-        throw ValueError(
-          format("%s (%d): A fraction cannot be evaluated with zero as denominator, y_in-y_base=%f ", __FILE__, __LINE__, y_in - y_base));
+    if ((y_exp < 0) && (std::abs(y_in - y_base) < CPPOLY_EPSILON)) {
+        // throw ValueError(format("%s (%d): A fraction cannot be evaluated with zero as denominator, y_in-y_base=%f ", __FILE__, __LINE__, y_in - y_base));
+        double y_lo = y_base - 1.001*CPPOLY_EPSILON;
+        double y_hi = y_base + 1.001*CPPOLY_EPSILON;
+        double z_lo = evaluate(coefficients, x_in, y_lo, x_exp, y_exp, x_base, y_base);
+        double z_hi = evaluate(coefficients, x_in, y_hi, x_exp, y_exp, x_base, y_base);
+        double dzdy = (z_hi - z_lo)/(y_hi - y_lo);
+        return z_lo + dzdy*(y_in - y_lo);
     }
 
     Eigen::MatrixXd tmpCoeffs(coefficients);
