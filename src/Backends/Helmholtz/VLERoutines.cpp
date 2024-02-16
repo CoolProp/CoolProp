CoolProp/CoolProp

commit 443a2fd276112a88ab42c3e74ce2aeb1c7b40764

Catch singular explosions in saturation_PHSU_pure (#2250)
* Catch singular explosions in saturation_PHSU_pure

Sometimes, the Akasaka solver has issues. This is usually caught and
then it is retried with a new omega. However, sometimes it goes bad
because the J matrix is singular, and this is not caught because the
error is not recalculated.

This commit recalculates the error term to prevent a bad result.

* Add comment to explain error check in saturation_PHSU_pure

* Add extra update check after saturation_PHSU_pure

During saturated PHSU flash calculations, SatL and SatV states have an
imposed phase. This is good for stability, but there is a small chance
that they can both up up with a matching third variable (e.g.,
pressure) that is not actually at the saturation point. This commit
forces a final DT update without this requirement. If an actual solution
has been found, the the error term will still be small. If not, then we
throw an exception and try again.

This continues work on #2245.

* Ensure that saturated phase is specified

In saturation_PHSU_pure, we unspecify the phase of SatL and SatV to
perform a final check. However, for any future updates, these states
*must* be set with specified phase. This commit ensures that no matter
what happens (exception, etc.) the phase is always specified again.

* Revert "Ensure that saturated phase is specified"

This reverts commit c6b650b861fe14d89d9d8f20b0b8a8312b2adcab.

The commit caused potential recursive lookups and did not solve the
issue at hand.

* Update the ammonio saturated rhoV ancillary

The rhoV ancillary gave somewhat wrong results. This commit provides a
closer fit that prevents errors downstream.

src/Backends/Helmholtz/VLERoutines.cpp
@@ -554,6 +554,52 @@ void SaturationSolvers::saturation_PHSU_pure(HelmholtzEOSMixtureBackend& HEOS, C
                                        info.c_str(), specified_value, error));
         }
     } while (error > 1e-9);
+    // Recalculate error
+    // The result has changed since the last error calculation.
+    // In rare scenarios, the final step can become unstable due to solving a singular
+    // J matrix. This final error check verifies that the solution is still good.
+    // Furthermore, the forced phase of SatL and SatV may have caused errors. We will recalculate them without this assumption.
+    SatL->unspecify_phase();
+    SatV->unspecify_phase();
+    SatL->update(DmolarT_INPUTS, rhoL, T);
+    SatV->update(DmolarT_INPUTS, rhoV, T);
+    negativer[0] = -(deltaV * (1 + deltaV * SatV->dalphar_dDelta()) - deltaL * (1 + deltaL * SatL->dalphar_dDelta()));
+    negativer[1] = -(deltaV * SatV->dalphar_dDelta() + SatV->alphar() + log(deltaV) - deltaL * SatL->dalphar_dDelta() - SatL->alphar() - log(deltaL));
+    switch (options.specified_variable) {
+        case saturation_PHSU_pure_options::IMPOSED_PL:
+            // -r_3 (equate calculated pressure and specified liquid pressure)
+            negativer[2] = -(SatL->p() / specified_value - 1);
+            break;
+        case saturation_PHSU_pure_options::IMPOSED_PV:
+            // -r_3 (equate calculated pressure and specified vapor pressure)
+            negativer[2] = -(SatV->p() / specified_value - 1);
+            break;
+        case saturation_PHSU_pure_options::IMPOSED_HL:
+            // -r_3 (equate calculated liquid enthalpy and specified liquid enthalpy)
+            negativer[2] = -(SatL->hmolar() - specified_value);
+            break;
+        case saturation_PHSU_pure_options::IMPOSED_HV:
+            // -r_3 (equate calculated vapor enthalpy and specified vapor enthalpy)
+            negativer[2] = -(SatV->hmolar() - specified_value);
+            break;
+        case saturation_PHSU_pure_options::IMPOSED_SL:
+            // -r_3 (equate calculated liquid entropy and specified liquid entropy)
+            negativer[2] = -(SatL->smolar() - specified_value);
+            break;
+        case saturation_PHSU_pure_options::IMPOSED_SV:
+            // -r_3 (equate calculated vapor entropy and specified vapor entropy)
+            negativer[2] = -(SatV->smolar() - specified_value);
+            break;
+        default:
+            throw ValueError(format("options.specified_variable to saturation_PHSU_pure [%d] is invalid", options.specified_variable));
+    }
+    error = sqrt(pow(negativer[0], 2) + pow(negativer[1], 2) + pow(negativer[2], 2));
+    // reset the phase for the next update.
+    SatL->specify_phase(iphase_liquid);
+    SatV->specify_phase(iphase_gas);
+    if (error > 1e-8){
+        throw SolutionError(format("saturation_PHSU_pure solver was good, but went bad. Current error is %Lg", error));
+    }
 }
 void SaturationSolvers::saturation_D_pure(HelmholtzEOSMixtureBackend& HEOS, CoolPropDbl rhomolar, saturation_D_pure_options& options)
 {

commit 6e6a80aca0f241696546feb5ccdca0c92e33c1bb

Fix bug introduced by 443a2fd
Commit 443a2fd addedsome extra checks for convergence in the Helmholtz
PHSU flash routines. However, its checking was more aggressive than the
original routine. This caused the routine to raise an exception on
several values that had previously been deemed acceptable. This trickled
down to cause the TTSE tables to be filled with empty values, making
them less robust.

This commit allows for a solution to be acceptable if the change is
small between iterations as was originally intended.

src/Backends/Helmholtz/VLERoutines.cpp
@@ -597,7 +597,7 @@ void SaturationSolvers::saturation_PHSU_pure(HelmholtzEOSMixtureBackend& HEOS, C
     // reset the phase for the next update.
     SatL->specify_phase(iphase_liquid);
     SatV->specify_phase(iphase_gas);
-    if (error > 1e-8){
+    if (error > 1e-8 && max_abs_value(v) > 1e-9){
         throw SolutionError(format("saturation_PHSU_pure solver was good, but went bad. Current error is %Lg", error));
     }
 }