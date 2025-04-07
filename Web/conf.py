63561bb79573705bf4dd5f834a22530afb97c37b
Ian Bell <ian.h.bell@gmail.com>
2025-04-06 21:38:48 -0400

    Slightly more helpful error message

--- Web/conf.py
+++ Web/conf.py
@@ -48,7 +48,7 @@ else:
                 # 'bbbinaries' : ('http://www.coolprop.dreamhosters.com:8010/binaries/%s',''),
                 # 'bbsphinx'   : ('http://www.coolprop.dreamhosters.com:8010/sphinx/%s','')
                 }
-print(extlinks)
+print(f'extlinks: {extlinks}')
 import sys, os, datetime
 
 # ~ # If your extensions are in another directory, add it here. If the directory
