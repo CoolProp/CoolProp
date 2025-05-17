746285e56637da830340335853336bef4314917d
Ian Bell <ian.h.bell@gmail.com>
2025-05-17 11:00:00 -0400

    Add valgrind files

---
+++ dev/docker/valgrind/run_all.py
@@ -0,0 +1,14 @@
+import subprocess
+
+with open('stdout','w') as fp:
+    subprocess.call('./CatchTestRunner --list-tags', shell=True, stdout=fp)
+
+with open('stdout') as fp:
+    tags = []
+    for line in fp.readlines()[2::]:
+        if line.strip():
+            tag = [t for t in line.split(' ') if t][1].strip()
+            tags.append(tag)
+
+for tag in tags:
+    subprocess.call('valgrind --tool=memcheck --error-limit=no --track-origins=yes ./CatchTestRunner '+tag, shell=True, cwd='.')
