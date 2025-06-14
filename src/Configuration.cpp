456d3654282ba086133a8417122d04b79021d425
Ian Bell <ian.h.bell@gmail.com>
2025-06-14 09:38:30 -0400

    Only initlalize the config if not already initialized

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -68,7 +68,9 @@ configuration_keys config_string_to_key(const std::string& s) {
 std::unique_ptr<Configuration> pconfig;
 /// A helper function to ensure that configuration is not accessed before it is initialized (was formerly static)
 Configuration* _get_config(){
-    pconfig = std::make_unique<Configuration>();
+    if (!pconfig){
+        pconfig = std::make_unique<Configuration>();
+    }
     return pconfig.get();
 }
 
