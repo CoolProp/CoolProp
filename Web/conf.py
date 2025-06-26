cf03991d0423ef9ee2f81b476a9c30816466056b
Ian Bell <ian.h.bell@gmail.com>
2025-06-26 17:09:44 -0400

    Force logo to exist
    See #2559

--- Web/conf.py
+++ Web/conf.py
@@ -263,6 +263,8 @@ edit_on_github_path_prefix = 'Web'
 # of the sidebar.
 html_logo = "_static/CoolPropLogo.png"
 
+assert(Path(html_logo).exists())
+
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
