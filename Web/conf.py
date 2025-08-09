572554f9e72a0353251086023e9e65d7e004edef
Ian Bell <ian.h.bell@gmail.com>
2025-08-09 09:02:11 -0400

    Fix mathjax path and add test

--- Web/conf.py
+++ Web/conf.py
@@ -124,7 +124,8 @@ if not zfile.exists():
     urllib.request.urlretrieve("https://github.com/mathjax/MathJax/archive/refs/tags/4.0.0.zip", zfile)
 with zipfile.ZipFile(zfile) as z:
     z.extractall(path=Path(__file__).parent / '_static')
-mathjax_path = "MathJax-master/tex-mml-chtml.js"
+mathjax_path = "MathJax-4.0.0/tex-mml-chtml.js"
+assert (Path(__file__).parent / '_static' / mathjax_path).exists()
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
