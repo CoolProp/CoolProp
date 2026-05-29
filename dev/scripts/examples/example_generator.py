e1441ee27c9f6dba0e97ded72f541da2c65ef645
Ian Bell <ian.h.bell@gmail.com>
2026-05-29 09:58:34 -0400

    fix(R): rename SWIG library to CoolPropR (#1674)

--- dev/scripts/examples/example_generator.py
+++ dev/scripts/examples/example_generator.py
@@ -803,7 +803,7 @@ class R(BaseParser):
         return l
 
     def header(self):
-        return 'dyn.load(paste("CoolProp", .Platform$dynlib.ext, sep=""))\nlibrary(methods) # See http://stackoverflow.com/a/19468533\nsource("CoolProp.R")\ncacheMetaData(1)\n'
+        return 'dyn.load(paste("CoolPropR", .Platform$dynlib.ext, sep=""))\nlibrary(methods) # See http://stackoverflow.com/a/19468533\nsource("CoolProp.R")\ncacheMetaData(1)\n'
 
 
 class MATLAB(BaseParser):
